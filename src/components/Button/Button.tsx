ff6fa1d996ecab3cd68ca481da4795c1d7f7d80a
Joshua Beitler <joshbeitler@gmail.com>
2019-11-13 17:58:24 -0700

    Create secondary and primary buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,70 @@ import React from "react";
 import styled from "styled-components";
 
 const AminoButton = styled.button`
-  background: var(--amino-primary);
-  padding: var(--amino-space-half) var(--amino-space);
-  color: white;
+  outline: none;
+  border: 1px solid transparent;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0 var(--amino-space);
   border-radius: var(--amino-radius);
+  transition: var(--amino-transition);
+
+  &:focus {
+    outline: none;
+  }
+
+  &[disabled] {
+    cursor: not-allowed;
+    pointer-events: none;
+    background: var(--amino-gray-lightest);
+  }
 `;
 
-export const Button: React.FC = props => {
-  return <AminoButton>{props.children}</AminoButton>;
+const Primary = styled(AminoButton)`
+  background: var(--amino-primary);
+  color: var(--amino-text-light);
+
+  &:hover {
+    background: var(--amino-primary-dark);
+  }
+`;
+
+const Secondary = styled(AminoButton)`
+  background: white;
+  color: var(--amino-text-dark);
+  border: 1px solid var(--amino-gray-light);
+
+  &:hover {
+    background: var(--amino-gray-lightest);
+  }
+`;
+
+type Props = {
+  intent?: string;
+  loading?: boolean;
+  disabled?: boolean;
+};
+
+export const Button: React.FC<Props> = ({
+  disabled,
+  children,
+  intent,
+  loading
+}) => {
+  const content = loading ? "loading..." : children;
+
+  if (disabled || loading) {
+    return <Secondary disabled>{content}</Secondary>
+  }
+
+  switch (intent) {
+    case "primary":
+      return <Primary>{content}</Primary>;
+    case "secondary":
+    default:
+      return <Secondary>{content}</Secondary>;
+  }
 };
