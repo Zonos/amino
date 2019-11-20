2e9a6ef61e58bc70cfa4c40a0914d4c9c3dcf607
Joshua Beitler <joshbeitler@gmail.com>
2019-11-20 15:09:45 -0700

    Input prefix + suffix

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,6 +3,25 @@ import styled from "styled-components";
 
 import { TextStyle, Text } from "../Text";
 
+const InputDecorator = styled.div`
+  height: 38px;
+  background: var(--amino-gray-lightest);
+  padding: 0 var(--amino-space);
+  border: 1px solid var(--amino-border-color);
+`;
+
+const InputPrefix = styled(InputDecorator)`
+  border-top-left-radius: var(--amino-radius);
+  border-bottom-left-radius: var(--amino-radius);
+  border-right: 0;
+`;
+
+const InputSuffix = styled(InputDecorator)`
+  border-top-right-radius: var(--amino-radius);
+  border-bottom-right-radius: var(--amino-radius);
+  border-left: 0;
+`;
+
 const AminoInput = styled.input`
   height: 38px;
   box-sizing: border-box;
@@ -10,15 +29,25 @@ const AminoInput = styled.input`
   outline: none;
   border: 1px solid var(--amino-border-color);
   padding: 0 var(--amino-space-half);
-  border-radius: var(--amino-radius);
   transition: var(--amino-transition);
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  width: 100%;
+  width: 100%;  
+  border-radius: var(--amino-radius);
 
   &:focus {
     outline: none;
     box-shadow: var(--amino-shadow-glow);
     border: 1px solid var(--amino-primary-light);
+  }
+  
+  &.has-prefix {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  
+  &.has-suffix {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
   }
 `;
 
@@ -29,6 +58,12 @@ const AminoInputWrapper = styled.div<any>`
   span {
     margin-top: var(--amino-space-quarter);
     display: block;
+  }
+
+  & div {
+    flex-direction: row;
+    align-items: center;
+    display: flex;
   }
 `;
 
@@ -50,6 +85,12 @@ type Props = {
 
   /** Input on changed. Required since all inputs must be fully controlled */
   onChange: (e: ChangeEvent<HTMLInputElement>) => any;
+
+  /** A short string displayed at the beginning of the input */
+  prefix?: string;
+
+  /** A short string displayed at the end of the input */
+  suffix?: string;
 };
 
 export const Input: React.FC<Props> = ({
@@ -58,16 +99,23 @@ export const Input: React.FC<Props> = ({
   placeholder,
   helpText,
   value,
-  onChange
+  onChange,
+  prefix,
+  suffix
 }) => {
   return (
     <AminoInputWrapper width={width} className="amino-input-wrapper">
       {label && <Text style={TextStyle.h5}>{label}</Text>}
-      <AminoInput
-        placeholder={placeholder || ""}
-        value={value}
-        onChange={onChange}
-      />
+      <div>
+        {prefix && <InputPrefix>{prefix}</InputPrefix>}
+        <AminoInput
+          className={`${prefix ? 'has-prefix' : ''} ${suffix ? 'has-suffix' : ''}`}
+          placeholder={placeholder || ""}
+          value={value}
+          onChange={onChange}
+        />
+        {suffix && <InputSuffix>{suffix}</InputSuffix>}
+      </div>
       {helpText && <Text style={TextStyle.Subtitle}>{helpText}</Text>}
     </AminoInputWrapper>
   );
