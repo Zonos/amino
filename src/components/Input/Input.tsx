78db06dbc70372ca8dccc165fe33304fcdf1ada6
Joshua Beitler <joshbeitler@gmail.com>
2019-11-20 14:28:32 -0700

    Input on change

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import styled from "styled-components";
 
 import { TextStyle, Text } from "../Text";
@@ -13,31 +13,56 @@ const AminoInput = styled.input`
   border-radius: var(--amino-radius);
   transition: var(--amino-transition);
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  width: 100%;
 
   &:focus {
     outline: none;
     box-shadow: var(--amino-shadow-glow);
     border: 1px solid var(--amino-primary-light);
   }
-
-  //&:active {
-  //  box-shadow: var(--amino-shadow-glow);
-  //}
 `;
 
-const AminoInputWrapper = styled.div`
+const AminoInputWrapper = styled.div<any>`
   position: relative;
+  width: ${p => (p.width ? `${p.width}px` : "100%")};
 `;
 
 type Props = {
+  /** A label that will be displayed above the input */
   label?: string;
+
+  /** A value (in px) that will determine how wide the input is. If nothing is passed, it defaults to 100% */
+  width?: number;
+
+  /** Placeholder text to be displayed in the input */
+  placeholder?: string;
+
+  /** Displayed in a tooltip next to the label */
+  helpText?: string;
+
+  /** Input value. Required since all inputs must be fully controlled */
+  value: string;
+
+  /** Input on changed. Required since all inputs must be fully controlled */
+  onChange: (e: ChangeEvent<HTMLInputElement>) => any;
 };
 
-export const Input: React.FC<Props> = ({ label }) => {
+export const Input: React.FC<Props> = ({
+  label,
+  width,
+  placeholder,
+  helpText,
+  value,
+  onChange
+}) => {
   return (
-    <AminoInputWrapper className="amino-input-wrapper">
+    <AminoInputWrapper width={width} className="amino-input-wrapper">
       {label && <Text style={TextStyle.h5}>{label}</Text>}
-      <AminoInput />
+      <AminoInput
+        placeholder={placeholder || ""}
+        value={value}
+        onChange={onChange}
+      />
     </AminoInputWrapper>
   );
 };
