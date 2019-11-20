3a49b6332df10804f3e03023ea330014faa4ed6e
Joshua Beitler <joshbeitler@gmail.com>
2019-11-20 16:50:02 -0700

    Remove layout padding

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,9 +3,6 @@ import styled from "styled-components";
 
 const AminoLayout = styled.div`
   background: var(--amino-page-background);
-  
-  // TODO: only for testing
-  padding: 50px;
 `;
 
 export const Layout: React.FC = props => {
