14e5d50407682a70dee587ab4f33ed6016dc8449
Joshua Beitler <joshbeitler@gmail.com>
2019-11-20 18:13:54 -0700

    Undo

--- src/hooks/useInputValue.ts
+++ src/hooks/useInputValue.ts
@@ -3,16 +3,8 @@ import { useState, useCallback } from "react";
 export const useInputValue = (initialValue: string) => {
   const [value, setValue] = useState(initialValue);
 
-  const onChange = useCallback((event, value) => {
-    if (event) {
-      setValue(event.currentTarget.value);
-      return;
-    }
-
-    if (value) {
-      setValue(value);
-      return;
-    }
+  const onChange = useCallback(event => {
+    setValue(event.currentTarget.value);
   }, []);
 
   return {
