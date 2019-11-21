8667148722af88d299b0bcf3104046b19470fc56
Joshua Beitler <joshbeitler@gmail.com>
2019-11-20 18:11:07 -0700

    Ability to set value on input hook

--- src/hooks/useInputValue.ts
+++ src/hooks/useInputValue.ts
@@ -1,10 +1,18 @@
 import { useState, useCallback } from "react";
 
 export const useInputValue = (initialValue: string) => {
-  let [value, setValue] = useState(initialValue);
+  const [value, setValue] = useState(initialValue);
 
-  let onChange = useCallback(function(event) {
-    setValue(event.currentTarget.value);
+  const onChange = useCallback((event, value) => {
+    if (event) {
+      setValue(event.currentTarget.value);
+      return;
+    }
+
+    if (value) {
+      setValue(value);
+      return;
+    }
   }, []);
 
   return {
