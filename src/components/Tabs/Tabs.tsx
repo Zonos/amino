9e77ea00001654cb5007d594b04bf640e142e8f8
Joshua Beitler <joshbeitler@gmail.com>
2020-08-07 15:23:02 -0600

    Use new variable syntax

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
+import { AminoTheme } from "../../styles/AminoTheme";
+
 const AminoTabs = styled.div`
   display: flex;
   align-items: center;
@@ -9,27 +11,27 @@ const AminoTabs = styled.div`
 
 const Tab = styled.div`
   flex: 1;
-  padding: var(--amino-space-half) 0;
+  padding: var(${AminoTheme.spaceHalf}) 0;
   cursor: pointer;
   text-align: center;
   transition: all 150ms ease-in-out;
   font-weight: 500;
-  color: var(--amino-gray-900);
+  color: var(${AminoTheme.gray900});
   user-select: none;
   border-bottom: 3px solid transparent;
 
   &.is-selected {
-    color: var(--amino-primary);
-    border-bottom: 3px solid var(--amino-primary);
+    color: var(${AminoTheme.primary});
+    border-bottom: 3px solid var(${AminoTheme.primary});
   }
 
   &:not(.is-selected):hover {
-    color: var(--amino-gray-900);
+    color: var(${AminoTheme.gray900});
     background: rgba(0, 0, 0, 0.03);
   }
 
   & + & {
-    margin-left: var(--amino-space-half);
+    margin-left: var(${AminoTheme.spaceHalf});
   }
 `;
 
