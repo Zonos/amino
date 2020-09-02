f2d572c57486cde21b742046a356ccc2b06a73dc
Joshua Beitler <joshbeitler@gmail.com>
2020-09-01 19:13:14 -0600

    Improve skeleton loaders

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,46 +1,18 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import { AminoTheme } from "../../styles/AminoTheme";
 
-const Pulse = keyframes`
-  from {
-    transform: scaleX(0);
-    opacity: 1;
-  } 
-  to {
-    opacity: 0;
-    transform: scaleX(1.5);
-  }
-`;
-
-const Pulser = styled.div`
-  animation: ${Pulse} 1.5s ease-in-out;
-  width: 100%;
-  height: 1em;
-  left: 0;
-  position: absolute;
-  animation-iteration-count: infinite;
-  transform-origin: top left;
-  background: rgb(255, 255, 255);
-  background: linear-gradient(
-    90deg,
-    rgba(255, 255, 255, 0.6264880952380952) 0%,
-    rgba(255, 255, 255, 1) 50%,
-    rgba(255, 255, 255, 0) 100%
-  );
-`;
-
-const SkeletonLoader = styled.div`
-  height: 1em;
-  width: 50px;
-  background: var(${AminoTheme.hoverColor});
+/**
+ * Displays a blocky outline of content that can be used as a loader
+ *
+ * @param width - Optional width in pixels
+ * @param height - Optional height in em
+ */
+export const Skeleton = styled.div<{ width?: number; height?: number }>`
+  height: ${p => p.height || 1}em;
+  width: ${p => `${p.width}px` || "100%"};
+  opacity: 0.3;
+  background: var(${AminoTheme.gray700});
   border-radius: var(${AminoTheme.radius});
   position: relative;
 `;
-
-export const Skeleton = () => (
-  <SkeletonLoader>
-    <Pulser />
-  </SkeletonLoader>
-);
