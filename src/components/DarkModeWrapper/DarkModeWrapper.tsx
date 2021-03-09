869f7cbc057bbda3370c22659030e2f45ec4f92d
Joshua Beitler <joshbeitler@gmail.com>
2021-03-08 21:04:51 -0700

    remove unused prop

--- src/components/DarkModeWrapper/DarkModeWrapper.tsx
+++ src/components/DarkModeWrapper/DarkModeWrapper.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 type Props = {
-  forceDarkMode?: boolean;
   children: React.ReactNode;
 };
 
-export const DarkModeWrapper = ({ forceDarkMode, children }: Props) => (
+export const DarkModeWrapper = ({ children }: Props) => (
   <div data-theme="dark">{children}</div>
 );
