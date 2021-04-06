d7c9459b059cc2baf83ce5b70333f959ddba1e7b
Joshua Beitler <joshbeitler@gmail.com>
2021-04-06 14:03:42 -0600

    Card

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
 import { Card, CardProps } from "../components/Card";
 import { Button } from "../components/Button";
 
 const CardMeta: Meta = {
   title: "Amino/Card",
-  component: Card
+  component: Card,
+  decorators: [withDesign],
 };
 
 export default CardMeta;
 
-const Template: Story<CardProps> = args => <Card {...args} />;
+const Template: Story<CardProps> = (args) => <Card {...args} />;
 
 export const BasicCard = Template.bind({});
 BasicCard.args = {
   children: "Card contents",
-  label: "My hot card"
+  label: "My hot card",
+};
+BasicCard.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=4%3A25",
+  },
 };
 
 export const CardWithActions = Template.bind({});
@@ -24,5 +33,12 @@ CardWithActions.args = {
   children: "Card contents",
   label: "My hot card",
   actions: <Button>Title action</Button>,
-  footerActions: <Button>Footer action</Button>
+  footerActions: <Button>Footer action</Button>,
+};
+CardWithActions.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=4%3A25",
+  },
 };
