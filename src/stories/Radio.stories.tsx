eb7162655cb11966eb50cce83bfe78f1fa0fb693
Joshua Beitler <joshbeitler@gmail.com>
2021-04-06 14:07:29 -0600

    Radio

--- src/stories/Radio.stories.tsx
+++ src/stories/Radio.stories.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
 import { Radio, RadioProps } from "../components/Radio";
 
 const RadioMeta: Meta = {
   title: "Amino/Radio",
-  component: Radio
+  component: Radio,
+  decorators: [withDesign],
 };
 
 export default RadioMeta;
 
-const Template: Story<RadioProps> = args => <Radio {...args} />;
+const Template: Story<RadioProps> = (args) => <Radio {...args} />;
 
 export const BasicRadio = Template.bind({});
 BasicRadio.args = {
-  label: "Example radio"
+  label: "Example radio",
+};
+BasicRadio.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=6%3A2",
+  },
 };
