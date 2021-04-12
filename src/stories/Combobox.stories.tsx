be9e4f51a92f2bd1b8449928afbf7a8f6e348db4
Joshua Beitler <joshbeitler@gmail.com>
2021-04-12 16:48:26 -0600

    New Figma links for toast

--- src/stories/Combobox.stories.tsx
+++ src/stories/Combobox.stories.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
 import { Combobox, ComboboxProps } from "../components/Combobox";
 
 const ComboboxMeta: Meta = {
   title: "Amino/Combobox",
-  component: Combobox
+  component: Combobox,
+  decorators: [withDesign],
 };
 
 export default ComboboxMeta;
 
-const Template: Story<ComboboxProps> = args => <Combobox {...args} />;
+const Template: Story<ComboboxProps> = (args) => <Combobox {...args} />;
 
 export const BasicCombobox = Template.bind({});
 BasicCombobox.args = {
@@ -18,15 +20,22 @@ BasicCombobox.args = {
   items: [
     {
       label: "Item A",
-      value: "A"
+      value: "A",
     },
     {
       label: "Item B",
-      value: "B"
+      value: "B",
     },
     {
       label: "Item C",
-      value: "C"
-    }
-  ]
+      value: "C",
+    },
+  ],
+};
+BasicCombobox.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=79%3A151",
+  },
 };
--- src/stories/Toast/Toast.stories.tsx
+++ src/stories/Toast/Toast.stories.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
 import { ToastContextProvider } from "../..";
 import { ToastConsumer } from "./ToastConsumer";
@@ -7,6 +8,7 @@ import { ToastConsumer } from "./ToastConsumer";
 const ToastContextProviderMeta: Meta = {
   title: "Amino/ToastContextProvider",
   component: ToastContextProvider,
+  decorators: [withDesign],
 };
 
 export default ToastContextProviderMeta;
@@ -18,3 +20,10 @@ const Template: Story = () => (
 );
 
 export const BasicToast = Template.bind({});
+BasicToast.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=79%3A162",
+  },
+};
