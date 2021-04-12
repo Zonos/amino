efc93c7c0cebcfa179b6026133cebabdd81d49a0
Joshua Beitler <joshbeitler@gmail.com>
2021-04-12 16:40:48 -0600

    New Figma links for menubutton

--- src/stories/MenuButton.stories.tsx
+++ src/stories/MenuButton.stories.tsx
@@ -5,15 +5,22 @@ import { MenuButton, MenuButtonProps } from "../components/Button";
 
 const MenuButtonMeta: Meta = {
   title: "Amino/MenuButton",
-  component: MenuButton
+  component: MenuButton,
 };
 
 export default MenuButtonMeta;
 
-const Template: Story<MenuButtonProps> = args => <MenuButton {...args} />;
+const Template: Story<MenuButtonProps> = (args) => <MenuButton {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   label: "Example menu button",
-  children: "Menu contents"
+  children: "Menu contents",
+};
+Default.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=79%3A89",
+  },
 };
