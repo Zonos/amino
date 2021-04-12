d41507915e2a1be5c4d8703538a3794335ac15be
Joshua Beitler <joshbeitler@gmail.com>
2021-04-12 16:46:06 -0600

    New Figma links for select

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
+import { withDesign } from "storybook-addon-designs";
 
 import { Select, SelectProps } from "../components/Select";
 
 const SelectMeta: Meta = {
   title: "Amino/Select",
-  component: Select
+  component: Select,
+  decorators: [withDesign],
 };
 
 export default SelectMeta;
 
-const Template: Story<SelectProps> = args => <Select {...args} />;
+const Template: Story<SelectProps> = (args) => <Select {...args} />;
 
 export const BasicSelect = Template.bind({});
 BasicSelect.args = {
@@ -18,15 +20,22 @@ BasicSelect.args = {
   items: [
     {
       label: "Item A",
-      value: "A"
+      value: "A",
     },
     {
       label: "Item B",
-      value: "B"
+      value: "B",
     },
     {
       label: "Item C",
-      value: "C"
-    }
-  ]
+      value: "C",
+    },
+  ],
+};
+BasicSelect.parameters = {
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=79%3A135",
+  },
 };
