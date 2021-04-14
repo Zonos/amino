b295951e4fee5fe1335ecfb67bf7061f39920bee
Joshua Beitler <joshbeitler@gmail.com>
2021-04-14 14:06:42 -0600

    Remove amino theme enum from Menu and MenuItem

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,17 +1,14 @@
-import React from "react";
 import styled from "styled-components";
 
-import { AminoTheme } from "../../styles/AminoTheme";
-
 export const MenuItem = styled.li`
-  padding: 0 var(${AminoTheme.spaceHalf});
+  padding: 0 var(--amino-space-half);
   height: 42px;
   line-height: 42px;
   user-select: none;
   cursor: default;
 
   &:hover {
-    background: var(${AminoTheme.hoverColor});
+    background: var(--amino-hover-color);
   }
 
   a {
