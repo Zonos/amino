11c2d5fb1d25e2a56e01ca5218a836df8bf32176
Joshua Beitler <joshbeitler@gmail.com>
2021-04-28 13:55:24 -0600

    Use CheckIcon internally

--- src/components/RichRadio/RichRadio.tsx
+++ src/components/RichRadio/RichRadio.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Root, Item, Indicator } from '@radix-ui/react-radio-group';
 
 import { VStack } from 'components/Stack';
+import { CheckIcon } from 'icons';
 
 const StyledItem = styled(Item)`
   appearance: none;
@@ -95,17 +96,7 @@ export const RichRadio = ({
             {item.subtitle && <Subtitle>{item.subtitle}</Subtitle>}
           </div>
           <StyledIndicator>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <CheckIcon />
           </StyledIndicator>
         </StyledItem>
       ))}
