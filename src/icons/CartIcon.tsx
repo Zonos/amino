0350305fdc53d384e64644191442c4c5bfdad126
Joshua Beitler <joshbeitler@gmail.com>
2021-05-03 15:31:15 -0600

    More icons

---
+++ src/icons/CartIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const CartIcon = () => (
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path
+      d="M2 2h2.12862l2.15709 8h5.99999L14 4.66667H5"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <circle cx="12" cy="13" r="1" fill="#000" />
+    <circle cx="6.5" cy="13" r="1" fill="#000" />
+  </svg>
+);
