1a4b407b01e1688df0220193db1b467d87c86eed
Joshua Beitler <joshbeitler@gmail.com>
2021-05-04 16:18:15 -0600

    less roundy

--- src/icons/CreditCardIcon.tsx
+++ src/icons/CreditCardIcon.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
 export const CreditCardIcon = () => (
-  <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
     <rect
-      x="1"
+      x="1.5"
       y="3"
-      width="14"
-      height="11"
-      rx="2"
+      width="13"
+      height="10"
+      rx="1"
       stroke="currentColor"
       strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
-    <path stroke="currentColor" strokeWidth="1.5" d="M15 6.75H1" />
+    <path stroke="currentColor" strokeWidth="1.5" d="M14 6.75H1" />
     <path
       stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M12.25 10.75h-2.5"
+      d="M11.75 9.75h-2.5"
     />
   </svg>
 );
