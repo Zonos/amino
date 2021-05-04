7ef5f9ae95a0ecce2f0c833eedfcfaa1a68f1853
Joshua Beitler <joshbeitler@gmail.com>
2021-05-04 14:44:15 -0600

    Arrows

---
+++ src/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ArrowLeftIcon = () => (
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path
+      d="M3 8l4-4M3 8l4 4M3 8h10"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ src/icons/ArrowRightIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ArrowRightIcon = () => (
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path
+      d="M13 8l-4 4m4-4L9 4m4 4H3"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
