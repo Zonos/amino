938d75353e3525d18e3c16019d9966b5bd67cce6
Joshua Beitler <joshbeitler@gmail.com>
2021-05-04 14:21:10 -0600

    More icons

---
+++ src/icons/CopyIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const CopyIcon = () => {
+  return (
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+      <rect
+        x="3"
+        y="2"
+        width="10"
+        height="12"
+        rx="1"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6 2h4v2c0 .55228-.44772 1-1 1H7c-.55228 0-1-.44772-1-1V2z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
+};
