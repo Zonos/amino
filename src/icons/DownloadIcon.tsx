ecd262e879eb7e4c7e8a160baa9cc3be43537038
Joshua Beitler <joshbeitler@gmail.com>
2021-05-04 14:59:20 -0600

    Download icon

---
+++ src/icons/DownloadIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const DownloadIcon = () => (
+  <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+    <path
+      d="M8 10L5 7.2M8 10l3-2.8M8 10V3M4.75 13.25h6.5"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
