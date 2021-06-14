3fbc03171c2d6abc85c2fec407e255f5406bcdfe
Robert Jensen <robert@zonos.com>
2021-06-14 10:46:14 -0600

    feat(hstack): add story

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -37,12 +37,6 @@ const CardFooter = styled.footer<{ footerHeight?: number }>`
   height: ${p => p.footerHeight && `${p.footerHeight}px`};
 `;
 
-const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: space-between;
-`;
-
 export type CardProps = {
   actions?: React.ReactNode;
   children: React.ReactNode;
@@ -64,22 +58,20 @@ export const Card = ({
 }: CardProps) => {
   return (
     <Surface depth={Depth.depth4} className={className || ''}>
-      <StyledWrapper>
-        {label && (
-          <CardHeader>
-            <Text type="h4">{label}</Text>
+      {label && (
+        <CardHeader>
+          <Text type="h4">{label}</Text>
 
-            <HStack spacing="space-quarter">{actions}</HStack>
-          </CardHeader>
-        )}
-        {children}
-        {(footerActions || footerContent) && (
-          <CardFooter footerHeight={footerHeight}>
-            <div>{footerContent}</div>
-            <HStack spacing="space-quarter">{footerActions}</HStack>
-          </CardFooter>
-        )}
-      </StyledWrapper>
+          <HStack spacing="space-quarter">{actions}</HStack>
+        </CardHeader>
+      )}
+      {children}
+      {(footerActions || footerContent) && (
+        <CardFooter footerHeight={footerHeight}>
+          <div>{footerContent}</div>
+          <HStack spacing="space-quarter">{footerActions}</HStack>
+        </CardFooter>
+      )}
     </Surface>
   );
 };
