72ece481e621b2ee81a1706f68c289c56b3ac79f
achung333 <xcessivepupil2@gmail.com>
2021-06-29 15:56:26 -0600

    fix(checkbox): remove extra onClick, add forwardRefs

--- src/icons/CheckIcon.tsx
+++ src/icons/CheckIcon.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { IconBase } from 'icons';
 import { IconProps } from 'types';
 
-export const CheckIcon = ({ size, color }: IconProps) => (
-  <IconBase size={size} color={color}>
-    <path
-      d="M2.5 7.5L6 12l7-7.5"
-      stroke="currentColor"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </IconBase>
+export const CheckIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ size, color }, ref) => (
+    <IconBase size={size} color={color} ref={ref}>
+      <path
+        d="M2.5 7.5L6 12l7-7.5"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </IconBase>
+  )
 );
