fb0909c7eb7a205ea60a8628193eafc729358dc5
Robert Jensen <robert@zonos.com>
2021-09-10 10:21:52 -0600

    refactor(skeleton): remove unused shimmer

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-
-const shimmerAnimation = (width: number) => keyframes`
-  0% {
-    left: -${width}px;
-  }
-  100% {
-    left: ${width}px;
-  }
-`;
+import styled from 'styled-components';
 
 export type SkeletonProps = {
   width?: number;
   height?: number;
-  noShimmer?: boolean;
 };
 
 /* animation: ${(p) => shimmerAnimation(p.width || 100)} 3s infinite; */
@@ -25,33 +15,12 @@ const SkeletonWrapper = styled.div<SkeletonProps>`
   background: var(--amino-gray-200);
 `;
 
-const SkeletonShimmer = styled.div<{ width: number }>`
-  width: 65%;
-  height: 100%;
-  display: block;
-  content: ' ';
-  background: var(--amino-gray-50);
-  animation: 2s infinite ${p => shimmerAnimation(p.width)};
-  animation-timing-function: ease-in-out;
-  position: absolute;
-  left: 0;
-
-  background: linear-gradient(
-    90deg,
-    rgba(0, 0, 0, 0) 0%,
-    rgba(255, 255, 255, 1) 50%,
-    rgba(0, 0, 0, 0) 100%
-  );
-`;
-
 /**
  * Displays a blocky outline of content that can be used as a loader
  *
  * @param width - Optional width in pixels
  * @param height - Optional height in em
  */
-export const Skeleton = ({ width, height, noShimmer }: SkeletonProps) => (
-  <SkeletonWrapper width={width} height={height}>
-    {/* {!noShimmer && <SkeletonShimmer width={width || 100} />} */}
-  </SkeletonWrapper>
+export const Skeleton = ({ width, height }: SkeletonProps) => (
+  <SkeletonWrapper width={width} height={height} />
 );
