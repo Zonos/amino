03c16f9ec40feec21a8afb1455448cf67b469794
Thien Huynh <thien@zonos.com>
2022-03-15 21:22:19 -0600

    refactor: box shadow and typoraghy (text scale)

--- src/components/Styles/Shadow.tsx
+++ src/components/Styles/Shadow.tsx
@@ -2,45 +2,46 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-enum ShadowEnum {
-  'Inset' = 'inset',
-  'Base' = 'base',
-  'Medium' = 'medium',
-  'Large' = 'large',
-  'XL' = 'xl',
-  '2XL' = 'xxl',
-}
+const shadows = [
+  { label: 'Inset', value: 'inset' },
+  { label: 'Base', value: 'small' },
+  { label: 'Medium', value: 'base' },
+  { label: 'Large', value: 'medium' },
+  { label: 'XL', value: 'large' },
+  { label: '2XL', value: 'larger' },
+] as const;
 
-const shadows: (keyof typeof ShadowEnum)[] = [
-  'Inset',
-  'Base',
-  'Medium',
-  'Large',
-  'XL',
-  '2XL',
-];
+type ShadowIntensity = typeof shadows[number]['value'];
+type AminoShadow = `--amino-shadow-${ShadowIntensity}`;
+
 interface ShadowProps {
-  shadow: keyof typeof ShadowEnum;
+  shadow: AminoShadow;
 }
 const StyledBoxShadowWrapper = styled.div<ShadowProps>`
   margin-bottom: var(--amino-space-double);
-  box-shadow: var(--amino-v3-shadow-${props => ShadowEnum[props.shadow]});
+  box-shadow: var(${props => props.shadow});
   background: var(--amino-gray-l80);
   padding: var(--amino-space-half) var(--amino-space);
 `;
 const StyledWrapper = styled.div`
   background: white;
-  padding: var(--amino-space);
+  padding: var(--amino-space-quarter) var(--amino-space);
 `;
 
 export const Shadow = () => {
   return (
     <StyledWrapper>
-      {shadows.map(shadow => (
-        <StyledBoxShadowWrapper shadow={shadow}>
-          <p>Shadow intensity: {shadow}</p>
-        </StyledBoxShadowWrapper>
-      ))}
+      {shadows.map(({ label, value }) => {
+        const aminoShadow: AminoShadow = `--amino-shadow-${value}`;
+        return (
+          <>
+            <p>Shadow intensity: {label}</p>
+            <StyledBoxShadowWrapper shadow={aminoShadow}>
+              <p>var({aminoShadow})</p>
+            </StyledBoxShadowWrapper>
+          </>
+        );
+      })}
     </StyledWrapper>
   );
 };
--- src/stories/BoxShadow.stories.tsx
+++ src/stories/BoxShadow.stories.tsx
@@ -3,18 +3,13 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 
 import { Shadow } from 'components/Styles/Shadow';
-import { Typo } from 'components/Styles/Typo';
 
 const StyleMeta: Meta = {
-  title: 'Amino',
+  title: 'Amino/BoxShadow',
 };
 
 export default StyleMeta;
 
-const TypoSizeTemplate: Story = () => <Typo />;
-
-export const TypoSize = TypoSizeTemplate.bind({});
-
 const BoxShadowTemplate: Story = () => <Shadow />;
 
-export const BoxShadow = BoxShadowTemplate.bind({});
+export const Demo = BoxShadowTemplate.bind({});
