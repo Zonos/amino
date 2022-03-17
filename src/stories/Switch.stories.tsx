cc92f5438216b69cad02bc0883dffbe8acc86b39
Thien Huynh <thien@zonos.com>
2022-03-16 23:23:35 -0600

    feat(switch): create switch component

---
+++ src/stories/Switch.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { withDesign } from 'storybook-addon-designs';
+
+import { Switch, SwitchProps } from '../components/Switch';
+
+const SwitchMeta: Meta = {
+  title: 'Amino/Switch',
+  component: Switch,
+  decorators: [withDesign],
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+      defaultValue: false,
+    },
+    subtitle: {
+      type: 'string',
+      defaultValue: 'test',
+    },
+  },
+};
+
+export default SwitchMeta;
+
+const Template: Story<SwitchProps> = ({
+  checked,
+  disabled,
+  label,
+  onChange,
+  subtitle,
+}: SwitchProps) => (
+  <Switch
+    checked={checked}
+    disabled={disabled}
+    label={label}
+    onChange={onChange}
+    subtitle={subtitle}
+  />
+);
+
+export const BasicSwitch = Template.bind({});
+BasicSwitch.args = {
+  label: 'Switch label',
+};
+BasicSwitch.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/dKbMcUDxYQ8INw5cUdvXLI/amino-tokens-2021?node-id=79%3A49',
+  },
+};
