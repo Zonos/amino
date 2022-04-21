90f329915ce2e5f064ba014204e1ec1194b10146
Robert Jensen <rjensen23@gmail.com>
2022-04-21 17:04:02 -0600

    fix(country-icon): always forward ref

--- src/i18n/CountryIcon.tsx
+++ src/i18n/CountryIcon.tsx
@@ -13,10 +13,10 @@ export const CountryIcon = forwardRef<SVGSVGElement, CountryIconProps>(
   ({ code, iconScale }, ref) => {
     const Icon = icons[code];
     if (iconScale === 'small') {
-      return <Icon width={16} height={12} />;
+      return <Icon ref={ref} width={16} height={12} />;
     }
     if (iconScale === 'medium') {
-      return <Icon width={20} height={15} />;
+      return <Icon ref={ref} width={20} height={15} />;
     }
 
     return <Icon ref={ref} width={32} height={24} />;
