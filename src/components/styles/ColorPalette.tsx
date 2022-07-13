8ee87bed661fee3f66cecffb639771f8ecb3011c
Robert Jensen <rjensen23@gmail.com>
2022-07-13 16:52:27 -0600

    feat(text): add color to text

--- src/components/styles/ColorPalette.tsx
+++ src/components/styles/ColorPalette.tsx
@@ -1,38 +1,9 @@
 import React from 'react';
 
 import { HStack } from 'src/components/stack/HStack';
+import { Color, colorContrasts, colorPrefixes } from 'src/types/Color';
 import styled from 'styled-components';
 
-const colors = [
-  'blue',
-  'cyan',
-  'gray',
-  'green',
-  'orange',
-  'purple',
-  'red',
-  'yellow',
-] as const;
-
-const contrastList = [
-  { label: 'L80', value: '100' },
-  { label: 'L60', value: '200' },
-  { label: 'L40', value: '300' },
-  { label: 'L20', value: '400' },
-  { label: 'Base', value: '500' },
-  { label: 'D20', value: '600' },
-  { label: 'D40', value: '700' },
-  { label: 'D60', value: '800' },
-  { label: 'D80', value: '900' },
-] as const;
-
-type ColorIntensity = typeof contrastList[number]['value'];
-type Color = typeof colors[number];
-type AminoColor = `--amino-${Color}-${ColorIntensity}`;
-
-interface ColorProps {
-  background: AminoColor;
-}
 const StyledWrapper = styled.div`
   margin-bottom: var(--amino-space);
 
@@ -40,7 +11,11 @@ const StyledWrapper = styled.div`
     height: 150px;
   }
 `;
-const StyledColorIntensity = styled.div<ColorProps>`
+type AminoColor = `--amino-${Color}`;
+
+const StyledColorIntensity = styled.div<{
+  background: AminoColor;
+}>`
   color: ${p => p.color};
   font-size: var(--amino-v3-text-sm);
   padding: var(--amino-space);
@@ -49,11 +24,11 @@ const StyledColorIntensity = styled.div<ColorProps>`
 
 export const ColorPalette = () => (
   <>
-    {colors.map(color => (
+    {colorPrefixes.map(color => (
       <StyledWrapper key={color}>
         <p>{color.toUpperCase()}</p>
         <HStack spacing="none">
-          {contrastList.map(({ label, value }) => {
+          {colorContrasts.map(({ label, value }) => {
             const aminoColor: AminoColor = `--amino-${color}-${value}`;
             return (
               <div key={aminoColor}>
--- src/types/Color.ts
+++ src/types/Color.ts
@@ -1,209 +1,26 @@
-export type GrayColor =
-  | 'gray-50'
-  | 'gray-100'
-  | 'gray-200'
-  | 'gray-300'
-  | 'gray-400'
-  | 'gray-500'
-  | 'gray-600'
-  | 'gray-700'
-  | 'gray-800'
-  | 'gray-900';
+export const colorPrefixes = [
+  'blue',
+  'cyan',
+  'gray',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow',
+] as const;
 
-export type GrayColorV2 =
-  | 'gray-l80'
-  | 'gray-l60'
-  | 'gray-l40'
-  | 'gray-l20'
-  | 'gray-base'
-  | 'gray-d20'
-  | 'gray-d40'
-  | 'gray-d60'
-  | 'gray-d80'
-  | 'gray-alpha';
+export const colorContrasts = [
+  { label: 'L80', value: 'l80' },
+  { label: 'L60', value: 'l60' },
+  { label: 'L40', value: 'l40' },
+  { label: 'L20', value: 'l20' },
+  { label: 'Base', value: 'base' },
+  { label: 'D20', value: 'd20' },
+  { label: 'D40', value: 'd40' },
+  { label: 'D60', value: 'd60' },
+  { label: 'D80', value: 'd80' },
+] as const;
 
-export type BlueColor =
-  | 'blue-50'
-  | 'blue-100'
-  | 'blue-200'
-  | 'blue-300'
-  | 'blue-400'
-  | 'blue-500'
-  | 'blue-600'
-  | 'blue-700'
-  | 'blue-800'
-  | 'blue-900';
-
-export type BlueColorV2 =
-  | 'blue-l80'
-  | 'blue-l60'
-  | 'blue-l40'
-  | 'blue-l20'
-  | 'blue-base'
-  | 'blue-d20'
-  | 'blue-d40'
-  | 'blue-d60'
-  | 'blue-d80'
-  | 'blue-alpha';
-
-export type CyanColor =
-  | 'cyan-50'
-  | 'cyan-100'
-  | 'cyan-200'
-  | 'cyan-300'
-  | 'cyan-400'
-  | 'cyan-500'
-  | 'cyan-600'
-  | 'cyan-700'
-  | 'cyan-800'
-  | 'cyan-900';
-
-export type CyanColorV2 =
-  | 'cyan-l80'
-  | 'cyan-l60'
-  | 'cyan-l40'
-  | 'cyan-l20'
-  | 'cyan-base'
-  | 'cyan-d20'
-  | 'cyan-d40'
-  | 'cyan-d60'
-  | 'cyan-d80'
-  | 'cyan-alpha';
-
-export type RedColor =
-  | 'red-50'
-  | 'red-100'
-  | 'red-200'
-  | 'red-300'
-  | 'red-400'
-  | 'red-500'
-  | 'red-600'
-  | 'red-700'
-  | 'red-800'
-  | 'red-900';
-
-export type RedColorV2 =
-  | 'red-l80'
-  | 'red-l60'
-  | 'red-l40'
-  | 'red-l20'
-  | 'red-base'
-  | 'red-d20'
-  | 'red-d40'
-  | 'red-d60'
-  | 'red-d80'
-  | 'red-alpha';
-
-export type OrangeColor =
-  | 'orange-50'
-  | 'orange-100'
-  | 'orange-200'
-  | 'orange-300'
-  | 'orange-400'
-  | 'orange-500'
-  | 'orange-600'
-  | 'orange-700'
-  | 'orange-800'
-  | 'orange-900';
-
-export type OrangeColorV2 =
-  | 'orange-l80'
-  | 'orange-l60'
-  | 'orange-l40'
-  | 'orange-l20'
-  | 'orange-base'
-  | 'orange-d20'
-  | 'orange-d40'
-  | 'orange-d60'
-  | 'orange-d80'
-  | 'orange-alpha';
-
-export type GreenColor =
-  | 'green-50'
-  | 'green-100'
-  | 'green-200'
-  | 'green-300'
-  | 'green-400'
-  | 'green-500'
-  | 'green-600'
-  | 'green-700'
-  | 'green-800'
-  | 'green-900';
-
-export type GreenColorV2 =
-  | 'green-l80'
-  | 'green-l60'
-  | 'green-l40'
-  | 'green-l20'
-  | 'green-base'
-  | 'green-d20'
-  | 'green-d40'
-  | 'green-d60'
-  | 'green-d80'
-  | 'green-alpha';
-
-export type YellowColor =
-  | 'yellow-50'
-  | 'yellow-100'
-  | 'yellow-200'
-  | 'yellow-300'
-  | 'yellow-400'
-  | 'yellow-500'
-  | 'yellow-600'
-  | 'yellow-700'
-  | 'yellow-800'
-  | 'yellow-900';
-
-export type YellowColorV2 =
-  | 'yellow-l80'
-  | 'yellow-l60'
-  | 'yellow-l40'
-  | 'yellow-l20'
-  | 'yellow-base'
-  | 'yellow-d20'
-  | 'yellow-d40'
-  | 'yellow-d60'
-  | 'yellow-d80'
-  | 'yellow-alpha';
-
-export type PurpleColor =
-  | 'purple-50'
-  | 'purple-100'
-  | 'purple-200'
-  | 'purple-300'
-  | 'purple-400'
-  | 'purple-500'
-  | 'purple-600'
-  | 'purple-700'
-  | 'purple-800'
-  | 'purple-900';
-
-export type PurpleColorV2 =
-  | 'purple-l80'
-  | 'purple-l60'
-  | 'purple-l40'
-  | 'purple-l20'
-  | 'purple-base'
-  | 'purple-d20'
-  | 'purple-d40'
-  | 'purple-d60'
-  | 'purple-d80'
-  | 'purple-alpha';
-
-export type Color =
-  | GrayColor
-  | GrayColorV2
-  | BlueColor
-  | BlueColorV2
-  | CyanColor
-  | CyanColorV2
-  | RedColor
-  | RedColorV2
-  | OrangeColor
-  | OrangeColorV2
-  | GreenColor
-  | GreenColorV2
-  | YellowColor
-  | YellowColorV2
-  | PurpleColor
-  | PurpleColorV2;
+type ColorContrast = typeof colorContrasts[number]['value'];
+type ColorPrefix = typeof colorPrefixes[number];
+export type Color = `${ColorPrefix}-${ColorContrast}`;
