cf470c9df019afebd05b7970458ddb3af8b4f384
Robert Jensen <rjensen23@gmail.com>
2022-07-13 17:08:54 -0600

    fix: dependency cycle

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,12 +3,5 @@ export type { Color } from './Color';
 export type { ConfirmDialogOpts } from './ConfirmDialogOpts';
 export type { IAminoTheme } from './IAminoTheme';
 export type { IconProps } from './IconProps';
-export type {
-  ICountry,
-  ICountryOption,
-  IGetCountriesResponse,
-  IRegionCountryOption,
-  IUnavailableCountry,
-} from './ICountry';
 export type { Intent } from './Intent';
 export type { StyledProps } from './StyledProps';
