0ab3e5283d5896afe01e9bacc5326ecf3f881855
Thien Huynh <thien@zonos.com>
2022-08-08 11:20:06 -0600

    refactor(eslint): remove unnecessary import

--- src/components/input/input-type/_FloatLabelInput.tsx
+++ src/components/input/input-type/_FloatLabelInput.tsx
@@ -1,6 +1,5 @@
 import React, { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
 
-import { theme } from 'src/styles/constants/theme';
 import { Size } from 'src/types/Size';
 import styled from 'styled-components';
 
