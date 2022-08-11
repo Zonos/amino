a5855f716a1d24f9a8778da93bdd72c0cb535625
Will Dickinson <will@zonos.com>
2022-08-11 13:36:35 -0600

    test: add render snapshots test

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const config: InitialOptionsTsJest = {
+  roots: ['<rootDir>'],
+  modulePaths: [
+    '<rootDir>',
+    'build-utils/**/*.ts',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    '.storybook/**/*.ts',
+  ],
+  modulePathIgnorePatterns: ['dist'],
+  setupFilesAfterEnv: ['./jest/setupJestMock.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
+};
+
+export default config;
---
+++ src/components/avatar/Avatar.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { ArrowDownIcon } from 'src/icons/ArrowDownIcon';
+
+import { DynamicIconAvatar } from './DynamicIconAvatar';
+import { IconAvatar } from './IconAvatar';
+import { ImageAvatar } from './ImageAvatar';
+import { UserAvatar } from './UserAvatar';
+
+describe('Avatar snapshot tests', () => {
+  it('UserAvatar matches snapshot', () => {
+    const tree = renderer
+      .create(<UserAvatar size="lg" shape="round" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('IconAvatar matches snapshot', () => {
+    const tree = renderer
+      .create(<IconAvatar icon={<ArrowDownIcon />} size="lg" shape="round" />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('ImageAvatar matches snapshot', () => {
+    const tree = renderer
+      .create(
+        <ImageAvatar
+          imageUrl="https://upload.wikimedia.org/wikipedia/commons/2/23/Mountain_Goat%2C_Enchantments_Basin.jpg"
+          size="lg"
+          shape="round"
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('DynamicIconAvatar matches snapshot', () => {
+    const tree = renderer
+      .create(
+        <DynamicIconAvatar icon="ArrowDownIcon" size="lg" shape="round" />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
