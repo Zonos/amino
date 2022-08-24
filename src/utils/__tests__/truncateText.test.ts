69b022b707f722e52d3efd74e2cdc2e8f4c36b89
Robert Jensen <rjensen23@gmail.com>
2022-08-24 16:11:23 -0600

    test(truncateText): test against typed params

--- src/utils/__tests__/truncateText.test.ts
+++ src/utils/__tests__/truncateText.test.ts
@@ -1,50 +1,50 @@
-import { truncateText } from '../truncateText';
+import { truncateText, TruncateTextParams } from '../truncateText';
 
 type TestCaseType = {
-  length: number;
-  ellipses: boolean;
-  text: string;
-  expected: string;
-};
+  expected: ReturnType<typeof truncateText>;
+} & TruncateTextParams;
 
 const testCases: TestCaseType[] = [
   {
     length: 30,
-    ellipses: true,
+    addEllipses: true,
     text: 'Tooltips should stay under 128 characters and be limited to two lines.',
     expected: 'Tooltips should stay under 128 ...',
   },
   {
     length: 25,
-    ellipses: true,
+    addEllipses: true,
     text: 'https://articulo.mercadolibre.com.mx/MLM-770177066-caminadora-electrica-3-hp-bluetooth-inclinacion-uso-rudo-app-_JM',
     expected: 'https://articulo.mercadol...',
   },
   {
     length: 30,
-    ellipses: false,
+    addEllipses: false,
     text: ' This example shows a tooltip with the max amount of characters.',
     expected: ' This example shows a tooltip ',
   },
   {
     length: 30,
-    ellipses: false,
+    addEllipses: false,
     text: 'https://articulo.mercadolibre.com.mx/MLM-770177066-caminadora-electrica-3-hp-bluetooth-inclinacion-uso-rudo-app-_JM',
     expected: 'https://articulo.mercadolibre.',
   },
   {
     length: 9,
-    ellipses: true,
+    addEllipses: true,
     text: '(435) 555-4327',
     expected: '(435) ...',
+  },
+  {
+    length: 9,
+    text: null,
+    expected: null,
   },
 ];
 
 test.each(testCases)(
   'returns truncated text correctly',
-  ({ text, length, ellipses, expected }) => {
-    expect(truncateText({ length, text, addEllipses: ellipses })).toBe(
-      expected
-    );
+  ({ text, length, addEllipses, expected }) => {
+    expect(truncateText({ length, text, addEllipses })).toBe(expected);
   }
 );
