d9881091adf75adf82ea8cb6fc4d005c78d8a050
Will Dickinson <will@zonos.com>
2022-09-09 14:09:12 -0600

    stories: improve spinner story

--- src/stories/Spinner.stories.tsx
+++ src/stories/Spinner.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { Spinner, SpinnerProps } from 'src/components/spinner/Spinner';
+import styled from 'styled-components';
 
 const SpinnerMeta: Meta = {
   title: 'Amino/Spinner',
@@ -10,8 +11,27 @@ const SpinnerMeta: Meta = {
 
 export default SpinnerMeta;
 
-const Template: Story<SpinnerProps> = ({ size }: SpinnerProps) => (
-  <Spinner size={size} />
+const Wrapper = styled.div<{ color: 'black' | 'white' | undefined }>`
+  width: min-content;
+  background-color: ${p => (p.color === 'white' ? 'black' : 'initial')};
+  border-radius: 5px;
+  padding: 10px;
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Template: Story<SpinnerProps> = ({
+  size,
+  color,
+  ...props
+}: SpinnerProps) => (
+  <Wrapper color={color}>
+    <Spinner size={size && size - 10} color={color} {...props} />
+    <Spinner size={size} color={color} {...props} />
+    <Spinner size={size && size + 10} color={color} {...props} />
+  </Wrapper>
 );
 
 export const BasicSpinner = Template.bind({});
