d09466ac3a36c8661b8b2ab4345737c7bffbd43b
Will Dickinson <will@zonos.com>
2022-09-12 11:03:28 -0600

    stories: button story improvement

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import { Meta, Story } from '@storybook/react/types-6-0';
 import { Button, ButtonProps } from 'src/components/button/Button';
+import { Text } from 'src/components/text/Text';
 import { CubeIcon } from 'src/icons/CubeIcon';
+import { theme } from 'src/styles/constants/theme';
 import { withDesign } from 'storybook-addon-designs';
 import styled from 'styled-components';
 
@@ -37,10 +39,13 @@ const ButtonMeta: Meta = {
   },
 };
 
+export default ButtonMeta;
+
 const HWrapper = styled.div`
   display: flex;
-  gap: 40px;
   flex-direction: row;
+  flex-wrap: wrap;
+  gap: ${theme.space};
 `;
 const VWrapper = styled.div`
   display: flex;
@@ -48,72 +53,89 @@ const VWrapper = styled.div`
   flex-direction: column;
 `;
 
-export default ButtonMeta;
+const ButtonRowLabel = styled(Text)`
+  border-bottom: 1px solid black;
+  margin-bottom: ${theme.spaceQuarter};
+`;
 
-const ButtonRow = ({ intent, disabled, loading, ...props }: ButtonProps) => (
-  <HWrapper>
-    <Button {...props} intent={intent} disabled={disabled} loading={loading} />
-    <Button
-      {...props}
-      icon={<CubeIcon size={16} />}
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-    />
-    <Button
-      {...props}
-      icon={<CubeIcon size={16} />}
-      iconRight
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-    />
-    <Button
-      {...props}
-      icon={<CubeIcon size={16} />}
-      /*  eslint-disable-next-line react/no-children-prop */
-      children=""
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-    />
-    <Button
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-      icon={<CubeIcon size={16} />}
-      /*  eslint-disable-next-line react/no-children-prop */
-      children=""
-      onClick={e => e.preventDefault()}
-      tag="div"
-    />
-    <StyledButton
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-      tag="div"
-      onClick={e => e.preventDefault()}
-    >
-      Div Button
-    </StyledButton>
-    <Button
-      intent={intent}
-      disabled={disabled}
-      loading={loading}
-      theme="dark"
-      {...props}
-    >
-      Dark
-    </Button>
-  </HWrapper>
+const ButtonRow = ({
+  label,
+  intent,
+  disabled,
+  loading,
+  ...props
+}: ButtonProps & { label: string }) => (
+  <div>
+    <ButtonRowLabel type="bold-subheader">{label}</ButtonRowLabel>
+    <HWrapper>
+      <Button<'button'>
+        {...props}
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+      />
+      <Button
+        {...props}
+        icon={<CubeIcon size={16} />}
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+      />
+      <Button
+        {...props}
+        icon={<CubeIcon size={16} />}
+        iconRight
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+      />
+      <Button
+        {...props}
+        icon={<CubeIcon size={16} />}
+        /*  eslint-disable-next-line react/no-children-prop */
+        children=""
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+      />
+      <Button
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+        icon={<CubeIcon size={16} />}
+        /*  eslint-disable-next-line react/no-children-prop */
+        children=""
+        onClick={e => e.preventDefault()}
+        tag="div"
+      />
+      <StyledButton
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+        tag="div"
+        onClick={e => e.preventDefault()}
+      >
+        Div Button
+      </StyledButton>
+      <Button
+        intent={intent}
+        disabled={disabled}
+        loading={loading}
+        theme="dark"
+        {...props}
+      >
+        Dark
+      </Button>
+    </HWrapper>
+  </div>
 );
 
 const Template: Story<ButtonProps> = props => {
   return (
     <VWrapper>
-      <ButtonRow {...props} />
-      <ButtonRow {...props} disabled />
-      <ButtonRow {...props} loading />
+      <ButtonRow {...props} label="Default" />
+      <ButtonRow {...props} disabled label="Disabled" />
+      <ButtonRow {...props} loading label="Loading" />
     </VWrapper>
   );
 };
