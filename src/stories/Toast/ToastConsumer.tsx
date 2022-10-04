98f8fd6893124a0b8df57e4b16f1dbeacdc8e979
Will Dickinson <will@zonos.com>
2022-10-04 13:09:43 -0600

    stories: add custom toast message input

--- src/stories/Toast/ToastConsumer.tsx
+++ src/stories/Toast/ToastConsumer.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Button } from 'src/components/button/Button';
 import { VStack } from 'src/components/stack/VStack';
 import { ToastContext } from 'src/components/toast/ToastContext';
+import { theme } from 'src/styles/constants/theme';
 import styled from 'styled-components';
 
 const useNotify = () => {
@@ -17,8 +18,19 @@ const LeftCenteredDiv = styled.div`
   align-items: center;
 `;
 
+const CustomWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${theme.space8};
+  border: ${theme.borderBlue};
+  border-radius: ${theme.radius6};
+  gap: 10px;
+`;
+
 export const ToastConsumer = () => {
   const notify = useNotify();
+
+  const [message, setMessage] = useState('Your custom message');
 
   return (
     <LeftCenteredDiv>
@@ -50,6 +62,23 @@ export const ToastConsumer = () => {
         >
           Information
         </Button>
+        <CustomWrapper>
+          <textarea
+            rows={5}
+            cols={30}
+            value={message}
+            onChange={v => setMessage(v.target.value)}
+            onKeyDown={e => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                notify(message, { intent: 'info' });
+              }
+            }}
+          />
+          <Button onClick={() => notify(message, { intent: 'info' })}>
+            Display Custom
+          </Button>
+        </CustomWrapper>
       </VStack>
     </LeftCenteredDiv>
   );
