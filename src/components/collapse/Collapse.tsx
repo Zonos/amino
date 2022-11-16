b378932ce48ae2702a17a279874d9e5b063af6f2
Will Dickinson <will@zonos.com>
2022-11-16 13:44:37 -0700

    stories: add vsection content to show padding and remove comments

--- src/components/collapse/Collapse.tsx
+++ src/components/collapse/Collapse.tsx
@@ -33,7 +33,6 @@ export const Collapse = ({
   const [height, setHeight] = useState(0);
   const [hideOverflow, setHideOverflow] = useState(!isExpand);
   const [resizeListener, sizes] = useResizeAware();
-  console.log(height, hideOverflow);
 
   useEffect(() => {
     setHideOverflow(true);
@@ -45,7 +44,6 @@ export const Collapse = ({
   }, [isExpand, setHeight, sizes.height, hideOverflow]);
 
   const handleTransitionEnd = () => {
-    console.log('transition ended', hideOverflow, isExpand);
     if (isExpand) {
       // Done expanding so safe to show overflow
       setHideOverflow(false);
