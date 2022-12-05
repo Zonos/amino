f7b70da40d68ddc9075da9143b5b96b23dc3f588
Thien Huynh <thien@zonos.com>
2022-12-05 14:58:47 -0700

    build: generate color constant from the svgs - Add generated colors - Import generated colors to the build `theme.ts` file. - Rebuild `theme.css` and `theme.ts` file

---
+++ build-utils/css/constants/generated/colors/_cyan.ts
@@ -0,0 +1,41 @@
+export const cyan = {
+  /** @info #E9FBFB  */
+  'cyan-100': '#E9FBFB',
+  /** @info #CBF6F4  */
+  'cyan-200': '#CBF6F4',
+  /** @info #9AEFEC  */
+  'cyan-300': '#9AEFEC',
+  /** @info #7BEAE7  */
+  'cyan-400': '#7BEAE7',
+  /** @info #4FE3DE  */
+  'cyan-500': '#4FE3DE',
+  /** @info #4AD3CF  */
+  'cyan-600': '#4AD3CF',
+  /** @info #44C5C1  */
+  'cyan-700': '#44C5C1',
+  /** @info #32A4A0  */
+  'cyan-800': '#32A4A0',
+  /** @info #225E5C  */
+  'cyan-900': '#225E5C',
+  /** @info #173B3A  */
+  'cyan-1000': '#173B3A',
+
+  /** @deprecated use cyan100 instead */
+  'cyan-l80': 'var(--amino-blue-100)',
+  /** @deprecated use cyan300 instead */
+  'cyan-l60': 'var(--amino-blue-300)',
+  /** @deprecated use cyan400 instead */
+  'cyan-l40': 'var(--amino-blue-400)',
+  /** @deprecated use cyan500 instead */
+  'cyan-l20': 'var(--amino-blue-500)',
+  /** @deprecated use cyan600 instead */
+  'cyan-base': 'var(--amino-blue-600)',
+  /** @deprecated use cyan700 instead */
+  'cyan-d20': 'var(--amino-blue-700)',
+  /** @deprecated use cyan800 instead */
+  'cyan-d40': 'var(--amino-blue-800)',
+  /** @deprecated use cyan900 instead */
+  'cyan-d60': 'var(--amino-blue-900)',
+  /** @deprecated use cyan1000 instead */
+  'cyan-d80': 'var(--amino-blue-1000)',
+} as const;
---
+++ build-utils/css/constants/generated/colors/_gray.ts
@@ -0,0 +1,49 @@
+export const gray = {
+  /** @info white  */
+  'gray-0': 'white',
+  /** @info #EFEFF0  */
+  'gray-100': '#EFEFF0',
+  /** @info #E4E4E7  */
+  'gray-200': '#E4E4E7',
+  /** @info #C8C8CB  */
+  'gray-300': '#C8C8CB',
+  /** @info #ABACB2  */
+  'gray-400': '#ABACB2',
+  /** @info #96979E  */
+  'gray-500': '#96979E',
+  /** @info #80818A  */
+  'gray-600': '#80818A',
+  /** @info #686970  */
+  'gray-700': '#686970',
+  /** @info #4F5257  */
+  'gray-800': '#4F5257',
+  /** @info #36383D  */
+  'gray-900': '#36383D',
+  /** @info #2B2C32  */
+  'gray-1000': '#2B2C32',
+  /** @info #1D1F24  */
+  'gray-1100': '#1D1F24',
+  /** @info #121417  */
+  'gray-1200': '#121417',
+  /** @info #0A0A0B  */
+  'gray-1300': '#0A0A0B',
+
+  /** @deprecated use gray100 instead */
+  'gray-l80': 'var(--amino-blue-100)',
+  /** @deprecated use gray300 instead */
+  'gray-l60': 'var(--amino-blue-300)',
+  /** @deprecated use gray400 instead */
+  'gray-l40': 'var(--amino-blue-400)',
+  /** @deprecated use gray500 instead */
+  'gray-l20': 'var(--amino-blue-500)',
+  /** @deprecated use gray600 instead */
+  'gray-base': 'var(--amino-blue-600)',
+  /** @deprecated use gray700 instead */
+  'gray-d20': 'var(--amino-blue-700)',
+  /** @deprecated use gray800 instead */
+  'gray-d40': 'var(--amino-blue-800)',
+  /** @deprecated use gray900 instead */
+  'gray-d60': 'var(--amino-blue-900)',
+  /** @deprecated use gray1000 instead */
+  'gray-d80': 'var(--amino-blue-1000)',
+} as const;
