ec7adb077095fabd1355dbd411604b8b5bce5251
Will Dickinson <will@zonos.com>
2023-01-12 16:14:56 -0700

    feat: better tabs

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,36 +1,80 @@
 import { theme } from 'src/styles/constants/theme';
 import styled from 'styled-components';
 
-const AminoTabs = styled.div`
+import { Text } from '../text/Text';
+
+const BaseTabs = styled.div`
   display: flex;
   align-items: center;
-  border: ${theme.border};
-  background: ${theme.surfaceColor};
-  border-radius: ${theme.radiusLg};
 `;
 
-const Tab = styled.div`
-  flex: 1;
-  padding: ${theme.spaceHalf} 0;
+const BaseTab = styled.div`
+  position: relative;
   cursor: pointer;
-  text-align: center;
-  transition: all 150ms ease-in-out;
-  font-weight: 500;
-  color: ${theme.grayD80};
   user-select: none;
-  box-sizing: border-box;
-  border-bottom: ${theme.radius} solid transparent;
+  text-align: center;
+  padding: ${theme.space12} 0;
+  transition: ${theme.transition};
+  color: ${theme.gray800};
+
+  &::after {
+    position: absolute;
+    bottom: 0px;
+    left: 0;
+    content: '';
+    background-color: ${theme.gray200};
+    transition: ${theme.transition};
+    height: 2px;
+    width: 100%;
+    transform: scaleX(0);
+  }
 
   &.is-selected {
     color: ${theme.primary};
-    border-bottom: ${theme.radius} solid ${theme.primary};
+
+    &::after {
+      background-color: ${theme.primary};
+      transform: scaleX(1);
+    }
   }
 
-  &:first-of-type {
-    border-bottom-left-radius: ${theme.radiusLg};
+  &:not(.is-selected):hover {
+    color: ${theme.gray1000};
+
+    &::after {
+      transform: scaleX(1);
+    }
   }
-  &:last-of-type {
-    border-bottom-right-radius: ${theme.radiusLg};
+
+  &:not(.is-selected):active {
+    color: ${theme.gray1000};
+
+    &::after {
+      background-color: ${theme.gray300};
+      transform: scaleX(1);
+    }
+  }
+`;
+
+const AminoTabs = styled(BaseTabs)`
+  border: ${theme.border};
+  border-radius: ${theme.radius8};
+`;
+
+const Tab = styled(BaseTab)`
+  flex: 1;
+  font-weight: 500;
+
+  &::after {
+    height: 4px;
+  }
+
+  &:first-of-type::after {
+    border-bottom-left-radius: ${theme.radius8};
+  }
+
+  &:last-of-type::after {
+    border-bottom-right-radius: ${theme.radius8};
   }
 
   & + & {
@@ -38,27 +82,75 @@ const Tab = styled.div`
   }
 
   &:not(.is-selected):hover {
-    color: ${theme.grayD80};
     background: rgba(0, 0, 0, 0.03);
   }
+
+  &:not(.is-selected):active {
+    background: rgba(0, 0, 0, 0.08);
+  }
 `;
+
+const SubtleTabs = styled(BaseTabs)<{ align: TabsProps['align'] }>`
+  justify-content: ${p => p.align};
+  gap: ${theme.space24};
+  border-bottom: 1px solid ${theme.gray200};
+`;
+
+const SubtleTab = BaseTab;
 
 export type TabsProps = {
   selected: number;
   onChange: (selectedTab: number) => void;
   items: string[];
+  /**
+   * Use the subtle design
+   * @default false
+   */
+  subtle?: boolean;
+  /**
+   * Only applies to subtle design
+   * Align tabs horizontally.
+   * @default 'start'
+   */
+  align?: 'start' | 'center' | 'end';
+  className?: string;
 };
 
-export const Tabs = ({ selected, onChange, items }: TabsProps) => (
-  <AminoTabs>
-    {items.map(item => (
-      <Tab
-        onClick={() => onChange(items.indexOf(item))}
-        className={selected === items.indexOf(item) ? 'is-selected' : ''}
-        key={item}
-      >
-        {item}
-      </Tab>
-    ))}
-  </AminoTabs>
-);
+export const Tabs = ({
+  selected,
+  onChange,
+  items,
+  subtle = false,
+  align = 'start',
+  className,
+}: TabsProps) => {
+  if (subtle) {
+    return (
+      <SubtleTabs align={align} className={className}>
+        {items.map(item => (
+          <SubtleTab
+            onClick={() => onChange(items.indexOf(item))}
+            className={selected === items.indexOf(item) ? 'is-selected' : ''}
+            key={item}
+          >
+            <Text type="label">{item}</Text>
+          </SubtleTab>
+        ))}
+      </SubtleTabs>
+    );
+  }
+
+  return (
+    <AminoTabs className={className}>
+      {items.map(item => (
+        <Tab
+          onClick={() => onChange(items.indexOf(item))}
+          className={selected === items.indexOf(item) ? 'is-selected' : ''}
+          key={item}
+        >
+          {item}
+        </Tab>
+      ))}
+    </AminoTabs>
+  );
+};
