98678e56696276b1760221d421127b86616d7dde
ckgardner <ckgard27@gmail.com>
2023-01-18 10:09:08 -0700

    product icons

---
+++ src/icons/LandedCostV2Icon.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from 'react';
+
+import { IconBase } from 'src/icons/icon-base/_IconBase';
+import type { IconProps } from 'src/types/IconProps';
+
+export const LandedCostV2Icon = forwardRef<SVGSVGElement, IconProps>(
+  ({ size, color, className }, ref) => (
+    <IconBase
+      ref={ref}
+      size={size}
+      color={color}
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M10.9 4.176A7.902 7.902 0 0 0 12 19.9a7.9 7.9 0 0 0 7.824-6.8H14.95c-.77 0-1.298-.61-1.357-1.25a1.6 1.6 0 0 0-1.444-1.443c-.639-.059-1.25-.588-1.25-1.357V4.176Zm.54-2.26c.945-.052 1.66.717 1.66 1.584v4.862a3.807 3.807 0 0 1 2.538 2.538H20.5c.867 0 1.636.715 1.585 1.66C21.793 17.878 17.39 22.1 12 22.1 6.422 22.1 1.9 17.578 1.9 12c0-5.39 4.222-9.793 9.54-10.085Z"
+        clipRule="evenodd"
+      />
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M14.425 4.818a1.1 1.1 0 0 1 1.48-.48 8.6 8.6 0 0 1 3.758 3.758 1.1 1.1 0 0 1-1.96.998 6.4 6.4 0 0 0-2.798-2.796 1.1 1.1 0 0 1-.48-1.48Z"
+        clipRule="evenodd"
+      />
+    </IconBase>
+  )
+);
---
+++ src/icons/LandedCostV2WhiteIcon.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from 'react';
+
+import { IconBase } from 'src/icons/icon-base/_IconBase';
+import type { IconProps } from 'src/types/IconProps';
+
+export const LandedCostV2WhiteIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ size, color, className }, ref) => (
+    <IconBase
+      ref={ref}
+      size={size}
+      color={color}
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M11 4.062A8.002 8.002 0 0 0 12 20a8.002 8.002 0 0 0 7.938-7H14.95c-.712 0-1.203-.565-1.257-1.158a1.7 1.7 0 0 0-1.535-1.535C11.565 10.253 11 9.762 11 9.05V4.062Zm.445-2.047C12.33 1.967 13 2.687 13 3.5v4.937A3.706 3.706 0 0 1 15.563 11H20.5c.814 0 1.533.67 1.485 1.555C21.697 17.82 17.337 22 12 22 6.477 22 2 17.523 2 12c0-5.337 4.18-9.696 9.445-9.985Z"
+        clipRule="evenodd"
+      />
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M14.514 4.863a1 1 0 0 1 1.345-.437 8.5 8.5 0 0 1 3.714 3.715 1 1 0 0 1-1.782.908 6.5 6.5 0 0 0-2.84-2.84 1 1 0 0 1-.437-1.346Z"
+        clipRule="evenodd"
+      />
+    </IconBase>
+  )
+);
