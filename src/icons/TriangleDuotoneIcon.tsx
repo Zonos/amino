5c9a30f8585db0b576f6f8e98b8356113968460d
ckgardner <ckgard27@gmail.com>
2023-01-18 14:47:02 -0700

    new icons

---
+++ src/icons/TriangleDuotoneIcon.tsx
@@ -0,0 +1,23 @@
+import { forwardRef } from 'react';
+
+import { IconBase } from 'src/icons/icon-base/_IconBase';
+import type { IconProps } from 'src/types/IconProps';
+
+export const TriangleDuotoneIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ size, color, className }, ref) => (
+    <IconBase
+      ref={ref}
+      size={size}
+      color={color}
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M10.257 5.06c.765-1.36 2.722-1.36 3.486 0l7.29 12.96c.75 1.333-.213 2.98-1.743 2.98H4.71c-1.53 0-2.493-1.647-1.743-2.98l7.29-12.96Z"
+        clipRule="evenodd"
+      />
+    </IconBase>
+  )
+);
