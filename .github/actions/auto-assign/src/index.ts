30a3b32d973668eae178a29bb9adcfae4c0d354c
Thien Huynh <thien@zonos.com>
2023-01-26 09:06:22 -0700

    feat: add engineer pool to reviewers list

--- .github/actions/auto-assign/src/index.ts
+++ .github/actions/auto-assign/src/index.ts
@@ -2,6 +2,8 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 import { PullRequestEvent } from '@octokit/webhooks-types';
 
+const engineerPool = ['evad1n', 'ckgardner', 'goodpickle', 'Thienhuynh95'];
+
 async function run() {
   try {
     const { payload, issue } = github.context;
@@ -20,6 +22,15 @@ async function run() {
     }
 
     const client = github.getOctokit(process.env.GITHUB_TOKEN).rest;
+    console.log('Trying to add reviewers', JSON.stringify(engineerPool));
+    const reviewerResult = await client.pulls.requestReviewers({
+      owner,
+      pull_number: issue_number,
+      repo,
+      /** Request all engineers that were not the author */
+      reviewers: engineerPool.filter(engineer => engineer !== author),
+    });
+    core.debug('Reviewer response: ' + JSON.stringify(reviewerResult));
 
     console.log('Trying to add assignees: ', JSON.stringify([author]));
 
