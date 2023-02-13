f991f1a44abbf415f16376d5d96f10ec7d4f454a
Will Dickinson <will@zonos.com>
2023-02-13 16:58:03 -0700

    feat: make divider default spacing 24

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -2,12 +2,12 @@ import { theme } from 'src/styles/constants/theme';
 import styled from 'styled-components';
 
 const DividerHorizontal = styled.hr`
-  margin: ${theme.space4} 0;
+  margin: ${theme.space24} 0;
   border-color: ${theme.borderColor};
 `;
 
 const DividerVertical = styled.hr`
-  margin: 0 ${theme.space4};
+  margin: 0 ${theme.space24};
   height: auto;
   align-self: stretch;
   border-right: solid thin ${theme.borderColor};
