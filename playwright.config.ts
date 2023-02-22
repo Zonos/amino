b383c7784aecdaeb2a87ca44fbcf81ae7e22b5c5
Thien Huynh <thien@zonos.com>
2023-02-22 08:49:48 -0700

    refactor: update config for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,9 +28,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
