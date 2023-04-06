25e9f0b49a0c539e41acd5f9f1a3765193190dda
ckgardner <ckgard27@gmail.com>
2023-04-06 14:49:38 -0600

    fix(lighbox): Changed noBorder to withBorder

--- src/components/lightbox/LightBox.tsx
+++ src/components/lightbox/LightBox.tsx
@@ -11,13 +11,13 @@ export type LightBoxProps = {
   open: boolean;
   theme?: IAminoTheme;
   width?: number;
-  noBorder?: boolean;
+  withBorder?: boolean;
   /** Close when clicking outside dialog (on the backdrop)
    * @param closeOnBlur
    * @default true
    */
   closeOnBlur?: boolean;
-  /** 
+  /**
    * Close on pressing 'escape' key
    * @param closeOnEsc
    * @default true
@@ -33,7 +33,7 @@ export const LightBox = forwardRef<HTMLDivElement, LightBoxProps>(
     open,
     theme: _theme,
     width,
-    noBorder = true,
+    withBorder = false,
     closeOnBlur,
     closeOnEsc,
   }) => (
@@ -42,7 +42,7 @@ export const LightBox = forwardRef<HTMLDivElement, LightBoxProps>(
       data-theme={_theme}
       open={open}
       width={width}
-      noBorder={noBorder}
+      noBorder={!withBorder}
       onClose={onClose}
       closeOnBlur={closeOnBlur}
       closeOnEsc={closeOnEsc}
