c3f7c35b912e744018d2de71fce464f9823c9eeb
Will Dickinson <will@zonos.com>
2023-05-15 11:23:10 -0600

    build: switch from yarn to pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -101,7 +101,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'pnpm dev',
     port: process.env.PLAYWRIGHT_TEST_BASE_URL ? undefined : 6006,
     url: process.env.PLAYWRIGHT_TEST_BASE_URL || undefined,
     ignoreHTTPSErrors: true,
