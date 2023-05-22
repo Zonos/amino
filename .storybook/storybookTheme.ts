decd08b334dde1b8eac089617b633987cf8ba196
ckgardner <ckgard27@gmail.com>
2023-05-22 13:25:49 -0600

    feat(theming): Revert storybook theme colors

--- .storybook/storybookTheme.ts
+++ .storybook/storybookTheme.ts
@@ -6,7 +6,7 @@ export default create({
   brandTitle: 'Building blocks for cross-border interfaces',
   brandUrl: 'https://amino.zonos.com',
   brandImage: 'https://zonos-docs.s3.us-east-1.amazonaws.com/amino-logo.png',
-  appBg: '#ffffff',
-  appContentBg: '#f7f7f7',
+  appBg: '#fafafa',
+  appContentBg: 'white',
   appBorderRadius: 8,
 });
