4e1b56932dd421787a00fcb4de7b9bc43609de9e
ckgardner <ckgard27@gmail.com>
2023-05-25 09:55:37 -0600

    feat(themeSwitch): Implemented 3 types of theme switchers

---
+++ src/components/theme-select/__stories__/ThemeSelect.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import styled from 'styled-components';
+
+import {
+  type Props,
+  ThemeSelect as ThemeSelectComponent,
+} from '../ThemeSelect';
+
+const ThemeSelectMeta: Meta = {
+  component: ThemeSelectComponent,
+};
+
+export default ThemeSelectMeta;
+
+const Wrapper = styled.div`
+  height: auto;
+  width: 360px;
+  margin: 0 auto;
+`;
+
+const Template: StoryFn = ({ type }: Props) => (
+  <Wrapper>
+    <ThemeSelectComponent type={type} />
+  </Wrapper>
+);
+
+export const ThemeSelect = Template.bind({});
+
+export const ThemeSelectCards = Template.bind({});
+ThemeSelectCards.args = { type: 'cards' };
+
+export const ThemeSelectToggle = Template.bind({});
+ThemeSelectToggle.args = { type: 'toggle' };
