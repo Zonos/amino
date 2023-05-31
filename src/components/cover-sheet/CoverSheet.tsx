101964f67c69ef3d14f0580217582a250cc18626
ckgardner <ckgard27@gmail.com>
2023-05-31 10:59:51 -0600

    fix(theme): brought portals back for dialog and cover sheet

--- src/components/cover-sheet/CoverSheet.tsx
+++ src/components/cover-sheet/CoverSheet.tsx
@@ -1,9 +1,12 @@
-import { type ReactNode, useEffect, useState } from 'react';
+import { type ReactNode, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { Text } from 'src/components/text/Text';
 import { RemoveIcon } from 'src/icons/RemoveIcon';
 import { theme } from 'src/styles/constants/theme';
+import type { Theme } from 'src/types';
+import { useAminoTheme } from 'src/utils/hooks/useAminoTheme';
 import styled from 'styled-components';
 
 import { Button } from '../button/Button';
@@ -21,6 +24,7 @@ const StyledDialog = styled(motion.div)`
   top: 0;
   width: 100vw;
   height: 100vh;
+  color: ${theme.textColor};
 
   @media print {
     height: unset;
@@ -65,6 +69,7 @@ export type CoverSheetProps = {
   /** used for setting id of the wrapper of where the action will be located */
   actionWrapperId?: string;
   actions?: ReactNode;
+  themeOverride?: Theme;
 };
 
 export const CoverSheet = ({
@@ -75,12 +80,9 @@ export const CoverSheet = ({
   children,
   actionWrapperId = '__cover-sheet-actions',
   actions,
+  themeOverride,
 }: CoverSheetProps) => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const { aminoTheme } = useAminoTheme();
 
   useEffect(() => {
     if (typeof document === 'undefined') {
@@ -94,37 +96,41 @@ export const CoverSheet = ({
     }
   }, [actionWrapperId]);
 
-  if (!isMounted) {
-    return null;
+  if (typeof document !== 'undefined') {
+    const body = document.querySelector('body');
+    if (body) {
+      return createPortal(
+        <AnimatePresence>
+          {open && (
+            <StyledDialog
+              className={className}
+              data-theme={themeOverride || aminoTheme}
+              transition={{ ease: [0.4, 0, 0.2, 1], duration: 0.35 }}
+              initial={{ opacity: 0, translateY: 10 }}
+              animate={{ opacity: 1, translateY: 0 }}
+              exit={{ opacity: 0, translateY: 5 }}
+            >
+              <Header>
+                <StyledCloseButton
+                  icon={<RemoveIcon size={20} />}
+                  onClick={onClose}
+                />
+                <StyledHeader type="header">{label}</StyledHeader>
+                <div id={actionWrapperId}>
+                  {actions && (
+                    <CoverSheetActions coverSheetActionId={actionWrapperId}>
+                      {actions}
+                    </CoverSheetActions>
+                  )}
+                </div>
+              </Header>
+              <Content>{children}</Content>
+            </StyledDialog>
+          )}
+        </AnimatePresence>,
+        body
+      );
+    }
   }
-
-  return (
-    <AnimatePresence>
-      {open && (
-        <StyledDialog
-          className={className}
-          transition={{ ease: [0.4, 0, 0.2, 1], duration: 0.35 }}
-          initial={{ opacity: 0, translateY: 10 }}
-          animate={{ opacity: 1, translateY: 0 }}
-          exit={{ opacity: 0, translateY: 5 }}
-        >
-          <Header>
-            <StyledCloseButton
-              icon={<RemoveIcon size={20} />}
-              onClick={onClose}
-            />
-            <StyledHeader type="header">{label}</StyledHeader>
-            <div id={actionWrapperId}>
-              {actions && (
-                <CoverSheetActions coverSheetActionId={actionWrapperId}>
-                  {actions}
-                </CoverSheetActions>
-              )}
-            </div>
-          </Header>
-          <Content>{children}</Content>
-        </StyledDialog>
-      )}
-    </AnimatePresence>
-  );
+  return null;
 };
