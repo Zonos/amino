efe94ca3d98070d1aba0e283190d6c1152be6719
Will Dickinson <will@zonos.com>
2023-06-07 15:28:59 -0600

    refactor: theme hook takes a root prop to automatically modify the body with the current theme

--- src/utils/hooks/useAminoTheme.ts
+++ src/utils/hooks/useAminoTheme.ts
@@ -1,12 +1,27 @@
+import { useEffect } from 'react';
+
 import type { Theme } from 'src/types';
 
 import { useStorage } from './useStorage';
 
-export const useAminoTheme = () => {
+type Params = {
+  /** Whether to modify the HTML body */
+  root: boolean;
+};
+
+export const useAminoTheme = (props?: Params) => {
+  const isRoot = !!props?.root;
   const [aminoTheme, setAminoTheme] = useStorage<Theme, 'amino:theme'>({
     defaultValue: 'day',
     key: 'amino:theme',
     type: 'local',
   });
+
+  useEffect(() => {
+    if (isRoot) {
+      document.body.dataset.theme = aminoTheme;
+    }
+  }, [aminoTheme, isRoot]);
+
   return { aminoTheme, setAminoTheme };
 };
