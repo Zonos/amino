c537cdbd93f9346eb831d9f894e26cb0a49f28e8
ckgardner <ckgard27@gmail.com>
2023-06-23 11:08:44 -0600

    fix(icons): fixed product icons and added thumbnail outline

--- src/icons/custom/products/RateIcon.tsx
+++ src/icons/custom/products/RateIcon.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react';
 
+import { useStableUniqueId } from 'src/icons/flag-icon/useStableUniqueId';
+
 import { IconBase } from '../../icon-base/_IconBase';
 
 type Props = {
@@ -7,123 +9,126 @@ type Props = {
   viewBox?: string;
 };
 export const RateIcon = forwardRef<SVGSVGElement, Props>(
-  ({ size = 24, viewBox }, ref) => (
-    <IconBase ref={ref} size={size} viewBox={viewBox}>
-      <mask
-        height="20"
-        id="mask0_4150_91294"
-        maskUnits="userSpaceOnUse"
-        style={{ maskType: 'alpha' }}
-        width="18"
-        x="3"
-        y="2"
-      >
-        <path
-          d="M3 20.2336V3.76643C3 3.06638 3.56638 2.5 4.26643 2.5H18.9395C20.0687 2.5 20.6351 3.86493 19.8365 4.66349L17.6922 6.80785C19.0214 8.13709 19.8435 9.97341 19.8435 12.0018C19.8435 14.0301 19.0214 15.8664 17.6922 17.1957L19.833 19.3365C20.628 20.1351 20.0652 21.5 18.9359 21.5H4.26643C3.56638 21.5 3 20.9336 3 20.2336Z"
-          fill="url(#paint0_linear_4150_91294)"
-        />
-      </mask>
-      <g mask="url(#mask0_4150_91294)">
-        <path
-          d="M12.4982 19.3471C16.5549 19.3471 19.8436 16.0585 19.8436 12.0018C19.8436 7.94507 16.5549 4.65646 12.4982 4.65646C8.44155 4.65646 5.15294 7.94507 5.15294 12.0018C5.15294 16.0585 8.44155 19.3471 12.4982 19.3471Z"
-          fill="url(#paint1_linear_4150_91294)"
-        />
-        <path
-          d="M3 3.76995V20.2336C3 20.9336 3.56638 21.5 4.26643 21.5H18.9359C20.0652 21.5 20.628 20.1351 19.833 19.3365L15.4075 14.911C16.1533 14.1652 16.6141 13.138 16.6141 11.9982C16.6141 10.8585 16.1533 9.83475 15.411 9.08896L19.8365 4.66349C20.6351 3.86493 20.0687 2.5 18.9395 2.5H4.26643C3.56638 2.5 3 3.06638 3 3.76643V3.76995Z"
-          fill="url(#paint2_linear_4150_91294)"
-        />
-        <g opacity="0.5">
-          <g filter="url(#filter0_f_4150_91294)">
-            <path
-              d="M11.5904 18.6957C15.2873 18.6957 18.2843 15.6987 18.2843 12.0018C18.2843 8.30484 15.2873 5.30789 11.5904 5.30789C7.89344 5.30789 4.89649 8.30484 4.89649 12.0018C4.89649 15.6987 7.89344 18.6957 11.5904 18.6957Z"
-              fill="#061238"
-            />
-          </g>
-          <g filter="url(#filter1_f_4150_91294)">
-            <path
-              d="M12.4561 18.6957C16.153 18.6957 19.15 15.6987 19.15 12.0018C19.15 8.30484 16.153 5.30789 12.4561 5.30789C8.75919 5.30789 5.76224 8.30484 5.76224 12.0018C5.76224 15.6987 8.75919 18.6957 12.4561 18.6957Z"
-              fill="#FFE37D"
-            />
+  ({ size = 24, viewBox }, ref) => {
+    const ids = useStableUniqueId(5);
+    return (
+      <IconBase ref={ref} size={size} viewBox={viewBox}>
+        <mask
+          height="20"
+          id={`${ids[0]}`}
+          maskUnits="userSpaceOnUse"
+          style={{ maskType: 'alpha' }}
+          width="18"
+          x="3"
+          y="2"
+        >
+          <path
+            d="M3 20.2336V3.76643C3 3.06638 3.56638 2.5 4.26643 2.5H18.9395C20.0687 2.5 20.6351 3.86493 19.8365 4.66349L17.6922 6.80785C19.0214 8.13709 19.8435 9.97341 19.8435 12.0018C19.8435 14.0301 19.0214 15.8664 17.6922 17.1957L19.833 19.3365C20.628 20.1351 20.0652 21.5 18.9359 21.5H4.26643C3.56638 21.5 3 20.9336 3 20.2336Z"
+            fill={`url(#${ids[1]})`}
+          />
+        </mask>
+        <g mask={`url(#${ids[0]})`}>
+          <path
+            d="M12.4982 19.3471C16.5549 19.3471 19.8436 16.0585 19.8436 12.0018C19.8436 7.94507 16.5549 4.65646 12.4982 4.65646C8.44155 4.65646 5.15294 7.94507 5.15294 12.0018C5.15294 16.0585 8.44155 19.3471 12.4982 19.3471Z"
+            fill={`url(#${ids[2]})`}
+          />
+          <path
+            d="M3 3.76995V20.2336C3 20.9336 3.56638 21.5 4.26643 21.5H18.9359C20.0652 21.5 20.628 20.1351 19.833 19.3365L15.4075 14.911C16.1533 14.1652 16.6141 13.138 16.6141 11.9982C16.6141 10.8585 16.1533 9.83475 15.411 9.08896L19.8365 4.66349C20.6351 3.86493 20.0687 2.5 18.9395 2.5H4.26643C3.56638 2.5 3 3.06638 3 3.76643V3.76995Z"
+            fill={`url(#${ids[3]})`}
+          />
+          <g opacity="0.5">
+            <g filter={`url(#${ids[5]})`}>
+              <path
+                d="M11.5904 18.6957C15.2873 18.6957 18.2843 15.6987 18.2843 12.0018C18.2843 8.30484 15.2873 5.30789 11.5904 5.30789C7.89344 5.30789 4.89649 8.30484 4.89649 12.0018C4.89649 15.6987 7.89344 18.6957 11.5904 18.6957Z"
+                fill="#061238"
+              />
+            </g>
+            <g filter={`url(#${ids[4]})`}>
+              <path
+                d="M12.4561 18.6957C16.153 18.6957 19.15 15.6987 19.15 12.0018C19.15 8.30484 16.153 5.30789 12.4561 5.30789C8.75919 5.30789 5.76224 8.30484 5.76224 12.0018C5.76224 15.6987 8.75919 18.6957 12.4561 18.6957Z"
+                fill="#FFE37D"
+              />
+            </g>
           </g>
         </g>
-      </g>
-      <defs>
-        <filter
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-          height="19.3878"
-          id="filter0_f_4150_91294"
-          width="19.3878"
-          x="1.89649"
-          y="2.30789"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            mode="normal"
-            result="shape"
-          />
-          <feGaussianBlur
-            result="effect1_foregroundBlur_4150_91294"
-            stdDeviation="1.5"
-          />
-        </filter>
-        <filter
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-          height="17.3878"
-          id="filter1_f_4150_91294"
-          width="17.3878"
-          x="3.76224"
-          y="3.30789"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            mode="normal"
-            result="shape"
-          />
-          <feGaussianBlur
-            result="effect1_foregroundBlur_4150_91294"
-            stdDeviation="1"
-          />
-        </filter>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint0_linear_4150_91294"
-          x1="22.1794"
-          x2="-9.07332"
-          y1="11.9771"
-          y2="12.044"
-        >
-          <stop stopColor="#2740F3" />
-          <stop offset="0.99" stopColor="#AA2DC9" />
-        </linearGradient>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint1_linear_4150_91294"
-          x1="13.6908"
-          x2="19.2877"
-          y1="12.0229"
-          y2="12.1214"
-        >
-          <stop stopColor="white" />
-          <stop offset="1" stopColor="#FFE37D" />
-        </linearGradient>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint2_linear_4150_91294"
-          x1="22.1794"
-          x2="-9.07332"
-          y1="11.9771"
-          y2="12.044"
-        >
-          <stop stopColor="#2740F3" />
-          <stop offset="0.99" stopColor="#AA2DC9" />
-        </linearGradient>
-      </defs>
-    </IconBase>
-  )
+        <defs>
+          <filter
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+            height="19.3878"
+            id={`${ids[5]}`}
+            width="19.3878"
+            x="1.89649"
+            y="2.30789"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              mode="normal"
+              result="shape"
+            />
+            <feGaussianBlur
+              result="effect1_foregroundBlur_4150_91294"
+              stdDeviation="1.5"
+            />
+          </filter>
+          <filter
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+            height="17.3878"
+            id={`${ids[4]}`}
+            width="17.3878"
+            x="3.76224"
+            y="3.30789"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              mode="normal"
+              result="shape"
+            />
+            <feGaussianBlur
+              result="effect1_foregroundBlur_4150_91294"
+              stdDeviation="1"
+            />
+          </filter>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[1]}`}
+            x1="22.1794"
+            x2="-9.07332"
+            y1="11.9771"
+            y2="12.044"
+          >
+            <stop stopColor="#2740F3" />
+            <stop offset="0.99" stopColor="#AA2DC9" />
+          </linearGradient>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[2]}`}
+            x1="13.6908"
+            x2="19.2877"
+            y1="12.0229"
+            y2="12.1214"
+          >
+            <stop stopColor="white" />
+            <stop offset="1" stopColor="#FFE37D" />
+          </linearGradient>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[3]}`}
+            x1="22.1794"
+            x2="-9.07332"
+            y1="11.9771"
+            y2="12.044"
+          >
+            <stop stopColor="#2740F3" />
+            <stop offset="0.99" stopColor="#AA2DC9" />
+          </linearGradient>
+        </defs>
+      </IconBase>
+    );
+  }
 );
--- src/icons/custom/products/ScreenIcon.tsx
+++ src/icons/custom/products/ScreenIcon.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react';
 
+import { useStableUniqueId } from 'src/icons/flag-icon/useStableUniqueId';
+
 import { IconBase } from '../../icon-base/_IconBase';
 
 type Props = {
@@ -7,123 +9,126 @@ type Props = {
   viewBox?: string;
 };
 export const ScreenIcon = forwardRef<SVGSVGElement, Props>(
-  ({ size = 24, viewBox }, ref) => (
-    <IconBase ref={ref} size={size} viewBox={viewBox}>
-      <mask
-        height="19"
-        id="mask0_4150_91295"
-        maskUnits="userSpaceOnUse"
-        style={{ maskType: 'alpha' }}
-        width="20"
-        x="2"
-        y="3"
-      >
-        <path
-          d="M17.4683 6.12989C16.0688 4.73963 14.1355 3.87974 12 3.87974C9.8645 3.87974 7.93116 4.73963 6.53171 6.12989L4.27778 3.89077C3.43704 3.05556 2 3.64794 2 4.82901V11.3599C2 16.7722 6.27037 21.3493 11.7185 21.4965C17.1667 21.6437 22 17.1438 22 11.5659V4.82901C22 3.64794 20.563 3.05924 19.7222 3.89077L17.4683 6.12989Z"
-          fill="url(#paint0_linear_4150_91295)"
-        />
-      </mask>
-      <g mask="url(#mask0_4150_91295)">
-        <path
-          d="M12 19.2447C16.271 19.2447 19.7333 15.8051 19.7333 11.5622C19.7333 7.3193 16.271 3.87973 12 3.87973C7.72899 3.87973 4.26666 7.3193 4.26666 11.5622C4.26666 15.8051 7.72899 19.2447 12 19.2447Z"
-          fill="url(#paint1_linear_4150_91295)"
-        />
-        <path
-          d="M15.0667 8.5194C14.2815 7.73938 13.2 7.25738 12 7.25738C10.8 7.25738 9.71852 7.73938 8.93704 8.5194L4.27778 3.89077C3.43704 3.05556 2 3.64794 2 4.82901V11.3599C2 16.7722 6.27037 21.3493 11.7185 21.4965C17.1667 21.6437 22 17.1438 22 11.5659V4.82901C22 3.64794 20.563 3.05924 19.7222 3.89077L15.063 8.5194H15.0667Z"
-          fill="url(#paint2_linear_4150_91295)"
-        />
-        <g opacity="0.65">
-          <g filter="url(#filter0_f_4150_91295)">
-            <path
-              d="M12 19.9806C16.271 19.9806 19.7333 16.541 19.7333 12.2981C19.7333 8.05515 16.271 4.61559 12 4.61559C7.72899 4.61559 4.26666 8.05515 4.26666 12.2981C4.26666 16.541 7.72899 19.9806 12 19.9806Z"
-              fill="#36490C"
-            />
-          </g>
-          <g filter="url(#filter1_f_4150_91295)">
-            <path
-              d="M12 19.2447C16.271 19.2447 19.7333 15.8051 19.7333 11.5622C19.7333 7.3193 16.271 3.87973 12 3.87973C7.72899 3.87973 4.26666 7.3193 4.26666 11.5622C4.26666 15.8051 7.72899 19.2447 12 19.2447Z"
-              fill="#FFE37D"
-            />
+  ({ size = 24, viewBox }, ref) => {
+    const ids = useStableUniqueId(5);
+    return (
+      <IconBase ref={ref} size={size} viewBox={viewBox}>
+        <mask
+          height="19"
+          id={`${ids[0]}`}
+          maskUnits="userSpaceOnUse"
+          style={{ maskType: 'alpha' }}
+          width="20"
+          x="2"
+          y="3"
+        >
+          <path
+            d="M17.4683 6.12989C16.0688 4.73963 14.1355 3.87974 12 3.87974C9.8645 3.87974 7.93116 4.73963 6.53171 6.12989L4.27778 3.89077C3.43704 3.05556 2 3.64794 2 4.82901V11.3599C2 16.7722 6.27037 21.3493 11.7185 21.4965C17.1667 21.6437 22 17.1438 22 11.5659V4.82901C22 3.64794 20.563 3.05924 19.7222 3.89077L17.4683 6.12989Z"
+            fill={`url(#${ids[1]})`}
+          />
+        </mask>
+        <g mask={`url(#${ids[0]})`}>
+          <path
+            d="M12 19.2447C16.271 19.2447 19.7333 15.8051 19.7333 11.5622C19.7333 7.3193 16.271 3.87973 12 3.87973C7.72899 3.87973 4.26666 7.3193 4.26666 11.5622C4.26666 15.8051 7.72899 19.2447 12 19.2447Z"
+            fill={`url(#${ids[2]})`}
+          />
+          <path
+            d="M15.0667 8.5194C14.2815 7.73938 13.2 7.25738 12 7.25738C10.8 7.25738 9.71852 7.73938 8.93704 8.5194L4.27778 3.89077C3.43704 3.05556 2 3.64794 2 4.82901V11.3599C2 16.7722 6.27037 21.3493 11.7185 21.4965C17.1667 21.6437 22 17.1438 22 11.5659V4.82901C22 3.64794 20.563 3.05924 19.7222 3.89077L15.063 8.5194H15.0667Z"
+            fill={`url(#${ids[3]})`}
+          />
+          <g opacity="0.65">
+            <g filter={`url(#${ids[5]})`}>
+              <path
+                d="M12 19.9806C16.271 19.9806 19.7333 16.541 19.7333 12.2981C19.7333 8.05515 16.271 4.61559 12 4.61559C7.72899 4.61559 4.26666 8.05515 4.26666 12.2981C4.26666 16.541 7.72899 19.9806 12 19.9806Z"
+                fill="#36490C"
+              />
+            </g>
+            <g filter={`url(#${ids[4]})`}>
+              <path
+                d="M12 19.2447C16.271 19.2447 19.7333 15.8051 19.7333 11.5622C19.7333 7.3193 16.271 3.87973 12 3.87973C7.72899 3.87973 4.26666 7.3193 4.26666 11.5622C4.26666 15.8051 7.72899 19.2447 12 19.2447Z"
+                fill="#FFE37D"
+              />
+            </g>
           </g>
         </g>
-      </g>
-      <defs>
-        <filter
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-          height="21.365"
-          id="filter0_f_4150_91295"
-          width="21.4667"
-          x="1.26666"
-          y="1.61559"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            mode="normal"
-            result="shape"
-          />
-          <feGaussianBlur
-            result="effect1_foregroundBlur_4150_91295"
-            stdDeviation="1.5"
-          />
-        </filter>
-        <filter
-          colorInterpolationFilters="sRGB"
-          filterUnits="userSpaceOnUse"
-          height="19.365"
-          id="filter1_f_4150_91295"
-          width="19.4667"
-          x="2.26666"
-          y="1.87973"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            mode="normal"
-            result="shape"
-          />
-          <feGaussianBlur
-            result="effect1_foregroundBlur_4150_91295"
-            stdDeviation="1"
-          />
-        </filter>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint0_linear_4150_91295"
-          x1="12"
-          x2="12"
-          y1="0.480012"
-          y2="18.0489"
-        >
-          <stop stopColor="#80BC17" />
-          <stop offset="1" stopColor="#FFD900" />
-        </linearGradient>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint1_linear_4150_91295"
-          x1="12"
-          x2="12"
-          y1="8.34279"
-          y2="5.01665"
-        >
-          <stop stopColor="white" />
-          <stop offset="1" stopColor="#FFE37D" />
-        </linearGradient>
-        <linearGradient
-          gradientUnits="userSpaceOnUse"
-          id="paint2_linear_4150_91295"
-          x1="12"
-          x2="12"
-          y1="0.480012"
-          y2="18.0489"
-        >
-          <stop stopColor="#80BC17" />
-          <stop offset="1" stopColor="#FFD900" />
-        </linearGradient>
-      </defs>
-    </IconBase>
-  )
+        <defs>
+          <filter
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+            height="21.365"
+            id={`${ids[5]}`}
+            width="21.4667"
+            x="1.26666"
+            y="1.61559"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              mode="normal"
+              result="shape"
+            />
+            <feGaussianBlur
+              result="effect1_foregroundBlur_4150_91295"
+              stdDeviation="1.5"
+            />
+          </filter>
+          <filter
+            colorInterpolationFilters="sRGB"
+            filterUnits="userSpaceOnUse"
+            height="19.365"
+            id={`${ids[4]}`}
+            width="19.4667"
+            x="2.26666"
+            y="1.87973"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              mode="normal"
+              result="shape"
+            />
+            <feGaussianBlur
+              result="effect1_foregroundBlur_4150_91295"
+              stdDeviation="1"
+            />
+          </filter>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[1]}`}
+            x1="12"
+            x2="12"
+            y1="0.480012"
+            y2="18.0489"
+          >
+            <stop stopColor="#80BC17" />
+            <stop offset="1" stopColor="#FFD900" />
+          </linearGradient>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[2]}`}
+            x1="12"
+            x2="12"
+            y1="8.34279"
+            y2="5.01665"
+          >
+            <stop stopColor="white" />
+            <stop offset="1" stopColor="#FFE37D" />
+          </linearGradient>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            id={`${ids[3]}`}
+            x1="12"
+            x2="12"
+            y1="0.480012"
+            y2="18.0489"
+          >
+            <stop stopColor="#80BC17" />
+            <stop offset="1" stopColor="#FFD900" />
+          </linearGradient>
+        </defs>
+      </IconBase>
+    );
+  }
 );
