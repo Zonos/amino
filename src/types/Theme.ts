1c19af7f80da985cd236de7261f83e7d9f60de48
Will Dickinson <will@zonos.com>
2023-07-03 12:38:10 -0600

    fix: theme initial load with swr

--- src/types/Theme.ts
+++ src/types/Theme.ts
@@ -1,1 +1,5 @@
-export type Theme = 'day' | 'night' | 'midnight';
+import { z } from 'zod';
+
+export const themeSchema = z.enum(['day', 'night', 'midnight']);
+
+export type Theme = z.infer<typeof themeSchema>;
