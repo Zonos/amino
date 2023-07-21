c15572727c8cfb828d7c59e76f3851911f787253
Will Dickinson <will@zonos.com>
2023-07-21 16:10:55 -0600

    lint: theme imports

--- build-utils/css/constants/theme/day/_shadow.ts
+++ build-utils/css/constants/theme/day/_shadow.ts
@@ -3,6 +3,8 @@ export const shadow = {
   'v3-shadow-medium': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 4px 6px rgba(10, 10, 11, 0.1), 0px 2px 4px rgba(10, 10, 11, 0.06)`,
   'v3-shadow-large': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 10px 15px rgba(10, 10, 11, 0.1), 0px 4px 6px rgba(10, 10, 11, 0.05)`,
   'v3-shadow-xl': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 20px 25px rgba(10, 10, 11, 0.1), 0px 10px 10px rgba(12, 12, 13, 0.04)`,
-  'v3-shadow-xxl': '0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 32px 50px rgba(10, 10, 11, 0.24)',
-  'v3-shadow-inset': '0px 1px 0px #FFFFFF, 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 1px 1px rgba(0, 0, 0, 0.04), inset 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 0px 0px 0.5px rgba(0, 0, 0, 0.06), inset 0px 0px 8px rgba(0, 0, 0, 0.02)',
+  'v3-shadow-xxl':
+    '0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 32px 50px rgba(10, 10, 11, 0.24)',
+  'v3-shadow-inset':
+    '0px 1px 0px #FFFFFF, 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 1px 1px rgba(0, 0, 0, 0.04), inset 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 0px 0px 0.5px rgba(0, 0, 0, 0.06), inset 0px 0px 8px rgba(0, 0, 0, 0.02)',
 } as const;
--- build-utils/css/constants/utils.ts
+++ build-utils/css/constants/utils.ts
@@ -3,14 +3,14 @@ type Invalid<T extends string = string> = {
 };
 
 export const constraintAminoVar = <
-  T extends Record<`--amino-${string}`, string>
+  T extends Record<`--amino-${string}`, string>,
 >(object: {
   [P in keyof T]: T[P];
 }) => object;
 
 export const constraintDefinedAminoVar = <
   U extends object,
-  T extends Record<string, string> = Record<string, string>
+  T extends Record<string, string> = Record<string, string>,
 >(
   _match: U,
   object: Exclude<
@@ -20,5 +20,5 @@ export const constraintDefinedAminoVar = <
         : Invalid<`Key need to match keys of the object passed in the first argument`>;
     },
     Invalid
-  >
+  >,
 ) => object;
