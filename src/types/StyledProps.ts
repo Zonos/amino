1cbfccc364bf7357a35bb4aff3b7f9e81f175f5b
Will Dickinson <will@zonos.com>
2023-08-07 15:00:58 -0600

    refactor: badge

--- src/types/StyledProps.ts
+++ src/types/StyledProps.ts
@@ -2,7 +2,6 @@
  * Prefix props with `$` to prevent them from being passed to native element or component.
  * @link https://styled-components.com/docs/api#using-custom-props
  */
-export type StyledProps<T, U = 'className'> = {
-  [Property in keyof T as `$${string &
-    Exclude<Property, U | 'className'>}`]?: T[Property];
+export type StyledProps<T> = {
+  [Property in keyof T as `$${string & Property}`]: T[Property];
 };
