66c732843a4b3bf7c447225b248d335bf7aa8bd8
Will Dickinson <will@zonos.com>
2023-08-07 14:20:43 -0600

    misc: add comment to styledProps

--- src/types/StyledProps.ts
+++ src/types/StyledProps.ts
@@ -1,3 +1,7 @@
+/**
+ * Prefix props with `$` to prevent them from being passed to native element or component.
+ * @link https://styled-components.com/docs/api#using-custom-props
+ */
 export type StyledProps<T, U = 'className'> = {
   [Property in keyof T as `$${string &
     Exclude<Property, U | 'className'>}`]?: T[Property];
