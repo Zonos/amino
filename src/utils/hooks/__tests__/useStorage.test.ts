a553999b0e2e8e4fc32bc585ac5574582b6cd682
Will Dickinson <will@zonos.com>
2023-09-13 13:44:28 -0600

    test: add more storage tests

--- src/utils/hooks/__tests__/useStorage.test.ts
+++ src/utils/hooks/__tests__/useStorage.test.ts
@@ -85,4 +85,72 @@ describe('useStorage', () => {
 
     expect(getCurrentValue()).toBeNull();
   });
+
+  test('Should validate a string union without a json parse', async () => {
+    const fruit = z.enum(['apple', 'orange', 'banana']);
+
+    const { result } = renderHook(() =>
+      useStorage({
+        defaultValue: 'apple',
+        key: 'people-array',
+        schema: fruit,
+        type: 'local',
+      }),
+    );
+
+    const getCurrentValue = () => result.current.value;
+
+    // Assert initial state
+    const { setValue, value: initialValue } = result.current;
+
+    expect(initialValue).toBe('apple');
+
+    await act(async () => {
+      await setValue('banana');
+    });
+
+    // Assert the updated state
+    expect(getCurrentValue()).toBe('banana');
+
+    // Manually mangle local storage
+    await act(async () => {
+      await setValue('ooga-booga' as unknown as z.infer<typeof fruit>);
+    });
+
+    expect(getCurrentValue()).toBe('apple');
+  });
+
+  test('Should parse a boolean without a schema', async () => {
+    const { result } = renderHook(() =>
+      useStorage({
+        defaultValue: false,
+        json: true,
+        key: 'boolean',
+        type: 'session',
+      }),
+    );
+
+    const getCurrentValue = () => result.current.value;
+
+    // Assert initial state
+    const { setValue, value: initialValue } = result.current;
+
+    expect(initialValue).toBe(false);
+
+    await act(async () => {
+      await setValue(true);
+    });
+
+    // Assert the updated state
+    expect(getCurrentValue()).toBe(true);
+
+    // Manually mangle local storage
+    await act(async () => {
+      await setValue('ooga-booga' as unknown as boolean);
+    });
+
+    // We have no schema, so it should just return the raw JSON parsed value
+    // Not sure why anyone would want this...
+    expect(getCurrentValue()).toBe('ooga-booga');
+  });
 });
