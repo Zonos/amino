69ee5837ef17012974179fec8f23ef615c38891d
Will Dickinson <will@zonos.com>
2023-09-26 16:07:13 -0600

    update grid spacing values

--- src/types/GridSpacing.ts
+++ src/types/GridSpacing.ts
@@ -1,2 +1,17 @@
 export type GridAlignment = 'start' | 'end';
-export type GridSpacing = 0 | 4 | 8 | 12 | 16 | 20 | 24 | 40;
+export type GridSpacing =
+  | 0
+  | 4
+  | 8
+  | 12
+  | 16
+  | 20
+  | 24
+  | 28
+  | 32
+  | 36
+  | 40
+  | 44
+  | 48
+  | 52
+  | 56;
