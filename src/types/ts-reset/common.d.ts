f1591960fb11a9f5b33a42437b1c8ea23d97d78b
ckgardner <ckgard27@gmail.com>
2023-10-05 14:33:04 -0600

    comments

--- src/types/ts-reset/common.d.ts
+++ src/types/ts-reset/common.d.ts
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface ObjectConstructor {
   keys<T extends Record<string, unknown>>(o: T): (keyof T)[];
 }
 
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 interface JSON {
   parse<T = unknown>(text: string): T;
 }
