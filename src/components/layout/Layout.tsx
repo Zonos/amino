fa11fe0e2eba1a62ffbc7e979971a019bd837b2e
Thien Huynh <thien@zonos.com>
2023-11-13 09:13:05 -0700

    refactor: Layout fully converted to scss modules

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,86 +1,12 @@
 import type { ChangeEventHandler, ReactElement, ReactNode } from 'react';
 
-import styled from 'styled-components';
-
 import { SearchInput } from 'src/components/input/SearchInput';
 import { type NavigationGroupProps } from 'src/components/layout/NavigationGroup';
 import { theme } from 'src/styles/constants/theme';
 import type { BaseProps } from 'src/types/BaseProps';
 
-const Footer = styled.div`
-  box-sizing: border-box;
-  width: ${theme.sidebarWidth};
-`;
-
-const SidebarContent = styled.div`
-  padding: ${theme.space16};
-  box-sizing: border-box;
-  overflow-y: auto;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`;
-
-const SearchInputWrapper = styled.div`
-  margin-bottom: ${theme.space40};
-`;
-const StyledLogoSidebar = styled.div`
-  margin-bottom: ${theme.space24};
-`;
-const StyledSearchInput = styled(SearchInput)`
-  border: 0;
-  svg {
-    color: ${theme.gray1200};
-  }
-  input {
-    background-color: ${theme.gray100};
-    border: 0;
-    ::placeholder {
-      color: ${theme.gray1200};
-    }
-  }
-`;
-const StyledSidebar = styled.nav`
-  border-right: ${theme.border};
-  width: ${theme.sidebarWidth};
-  box-sizing: border-box;
-  display: grid;
-  grid-template-rows: 1fr calc(39px + ${theme.space24} * 2);
-  height: inherit;
-  background: ${theme.sidebarColor};
-`;
-
-const ContentGrid = styled.div<{ $hasHeader: boolean }>`
-  height: ${p =>
-    p.$hasHeader ? `calc(100vh - ${theme.appbarHeight})` : '100vh'};
-  display: grid;
-  grid-template-columns: ${theme.sidebarWidth} 1fr;
-`;
-
-const AminoLayout = styled.main`
-  height: 100vh;
-  overflow: hidden;
-`;
-
-const Content = styled.div`
-  height: inherit;
-  overflow-y: auto;
-  padding: ${theme.space32};
-  box-sizing: border-box;
-`;
-
-const Header = styled.header`
-  background: ${theme.headerColor};
-  box-shadow: ${theme.v3ShadowBase};
-  border-bottom: ${theme.border};
-  height: ${theme.appbarHeight};
-  z-index: ${theme.appbarElevation};
-  position: sticky;
-  top: 0;
-  box-sizing: border-box;
-  display: flex;
-`;
+// the build config only work with css module to be imported relatively, we will do absolute import in the future
+import layoutStyles from './Layout.module.scss';
 
 type SearchInputProps = {
   onChange: ChangeEventHandler<HTMLInputElement>;
@@ -105,28 +31,36 @@ export const Layout = ({
   searchInput,
   sidebar,
 }: LayoutProps) => (
-  <AminoLayout className={className}>
-    {!!headerContent && <Header>{headerContent}</Header>}
-    <ContentGrid $hasHeader={!!headerContent}>
-      <StyledSidebar>
-        <SidebarContent>
+  <main className={[className, layoutStyles.aminoLayout].join(' ')}>
+    {!!headerContent && (
+      <header className={layoutStyles.header}>{headerContent}</header>
+    )}
+    <div
+      className={layoutStyles.contentGrid}
+      style={{
+        height: headerContent ? `calc(100vh - ${theme.appbarHeight})` : '100vh',
+      }}
+    >
+      <nav className={layoutStyles.styledSidebar}>
+        <div className={layoutStyles.sidebarContent}>
           {!!logoSidebar && (
-            <StyledLogoSidebar>{logoSidebar}</StyledLogoSidebar>
+            <div className={layoutStyles.styledLogoSidebar}>{logoSidebar}</div>
           )}
           {!!searchInput && (
-            <SearchInputWrapper>
-              <StyledSearchInput
+            <div className={layoutStyles.searchInputWrapper}>
+              <SearchInput
+                className={layoutStyles.styledSearchInput}
                 onChange={searchInput.onChange}
                 value={searchInput.value}
               />
-            </SearchInputWrapper>
+            </div>
           )}
           {sidebar}
-        </SidebarContent>
+        </div>
 
-        <Footer>{footer}</Footer>
-      </StyledSidebar>
-      <Content>{content}</Content>
-    </ContentGrid>
-  </AminoLayout>
+        <div className={layoutStyles.footer}>{footer}</div>
+      </nav>
+      <div className={layoutStyles.content}>{content}</div>
+    </div>
+  </main>
 );
