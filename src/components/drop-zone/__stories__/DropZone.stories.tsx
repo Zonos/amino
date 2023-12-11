36484e664ca593dc5ccae24582f65feaff62d027
Will Dickinson <will@zonos.com>
2023-12-11 14:13:50 -0700

    build: remove storyshots because it is getting deprecated

--- src/components/drop-zone/__stories__/DropZone.stories.tsx
+++ src/components/drop-zone/__stories__/DropZone.stories.tsx
@@ -1,14 +1,12 @@
 import { useContext, useState } from 'react';
 
 import type { Meta, StoryFn } from '@storybook/react';
-import type { Page } from 'puppeteer';
 
 import { DropZone as DropZoneComponent } from 'src/components/drop-zone/DropZone';
 import {
   ToastContext,
   ToastContextProvider,
 } from 'src/components/toast/ToastContext';
-import { customSnapshotsDir } from 'src/utils/_snapshotsFolder';
 
 const DropZoneMeta: Meta = {
   component: DropZoneComponent,
@@ -23,12 +21,6 @@ const DropZoneMeta: Meta = {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/WnKnmG7L3Q74hqPsw4rbEE/Amino-2.0?node-id=2612%3A72912&t=erzegCytT9AfSn9f-0',
-    },
-    async puppeteerTest(page: Page) {
-      const image = await page.screenshot({ fullPage: true });
-      expect(image).toMatchImageSnapshot({
-        customSnapshotsDir,
-      });
     },
   },
 };
--- src/components/styles/__stories__/Colors.stories.tsx
+++ src/components/styles/__stories__/Colors.stories.tsx
@@ -1,24 +1,16 @@
 import type { Meta } from '@storybook/react';
-import type { Page } from 'puppeteer';
 import styled from 'styled-components';
 
 import { VStack } from 'src/components/stack/VStack';
 import { Text } from 'src/components/text/Text';
 import { theme } from 'src/styles/constants/theme';
 import { type Color, colorContrasts, colorPrefixes } from 'src/types/Color';
-import { customSnapshotsDir } from 'src/utils/_snapshotsFolder';
 
 const meta: Meta = {
   parameters: {
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/WnKnmG7L3Q74hqPsw4rbEE/Amino-2.0?node-id=3576%3A73760&t=GYq7PyhsfdRYIayr-0',
-    },
-    async puppeteerTest(page: Page) {
-      const image = await page.screenshot({ fullPage: true });
-      expect(image).toMatchImageSnapshot({
-        customSnapshotsDir,
-      });
     },
   },
   title: 'Color Palette',
