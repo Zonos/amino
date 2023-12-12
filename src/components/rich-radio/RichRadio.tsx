41fbde92956890a92cb3cb953cb528769d803ef9
Nick Lanners <nick.lanners@zonos.com>
2023-12-12 16:31:23 -0700

    refactor(css): convert to css modules
    - RichRadio
    - RoundedIcon

--- src/components/rich-radio/RichRadio.tsx
+++ src/components/rich-radio/RichRadio.tsx
@@ -2,104 +2,12 @@ import { type ReactNode, useEffect, useState } from 'react';
 import ReactTooltip, { type TooltipProps } from 'react-tooltip';
 
 import clsx from 'clsx';
-import styled from 'styled-components';
 
 import { VStack } from 'src/components/stack/VStack';
 import { Text } from 'src/components/text/Text';
 import { CheckmarkIcon } from 'src/icons/CheckmarkIcon';
-import { theme } from 'src/styles/constants/theme';
 
 import styles from './RichRadio.module.scss';
-
-const StyledIcon = styled.div`
-  position: absolute;
-  content: ' ';
-  right: ${theme.space16};
-  background: ${theme.gray400};
-  border-radius: 50px;
-  padding: 5px;
-  svg {
-    color: ${theme.gray0};
-  }
-`;
-const StyledActiveIcon = styled.div`
-  position: absolute;
-  right: ${theme.space16};
-  background: ${theme.blue600};
-  content: ' ';
-  border-radius: 50px;
-  padding: 2px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const StyledItem = styled.button<{ $itemHeight: number }>`
-  position: relative;
-  appearance: none;
-  background: ${theme.gray0};
-  padding: ${theme.space16};
-  padding-right: calc(${theme.space40} + 10px);
-  border: ${theme.border};
-  border-radius: ${theme.radius6};
-  text-align: left;
-  transition: all 150ms ease-in-out;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: ${p => p.$itemHeight}px;
-  &:hover {
-    background: ${theme.gray100};
-    border: 1px solid ${theme.gray200};
-    ${StyledIcon} {
-      background: ${theme.gray600};
-    }
-  }
-
-  &:focus {
-    outline: none;
-    border: 1px solid ${theme.blue400};
-    box-shadow: 0 0 0 1px ${theme.blue400};
-  }
-  > div {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-  }
-`;
-
-const Subtitle = styled(Text)``;
-
-const Label = styled(Text)`
-  color: ${theme.textColor};
-  line-height: 24px;
-  [data-state='checked'] & {
-    color: ${theme.primary};
-  }
-`;
-
-const StyledRadioGroup = styled(VStack)`
-  ${StyledItem}[data-state='checked'] {
-    background: ${theme.blue100};
-    border: 1px solid ${theme.blue400};
-    box-shadow: 0 0 0 1px ${theme.blue400};
-    color: ${theme.blue700};
-    ${Subtitle} {
-      color: ${theme.blue600};
-    }
-  }
-  svg {
-    color: ${theme.gray0};
-    width: 16px;
-    height: 16px;
-  }
-`;
-const StyledTooltip = styled(ReactTooltip)`
-  && {
-    max-width: 350px;
-    border-radius: 10px;
-  }
-`;
 
 export type RichRadioItemType<T extends string> = {
   disabled?: boolean;
---
+++ src/components/rounded-icon/RoundedIcon.module.scss.d.ts
@@ -0,0 +1,5 @@
+export declare const danger: string;
+export declare const iconWrapper: string;
+export declare const primary: string;
+export declare const success: string;
+export declare const warning: string;
