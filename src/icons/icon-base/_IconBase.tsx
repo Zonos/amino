2471c5ef13b2b1109a8bc50deac124f4697d21cf
Nick Lanners <nick.lanners@zonos.com>
2023-12-14 11:32:05 -0700

    refactor(icon): change prop to `inlineBlock`

--- src/icons/icon-base/_IconBase.tsx
+++ src/icons/icon-base/_IconBase.tsx
@@ -6,14 +6,14 @@ import { type IconProps } from 'src/types/IconProps';
 type Props = { children: ReactNode; viewBox?: string } & IconProps;
 
 export const IconBase = forwardRef<SVGSVGElement, Props>(
-  ({ children, className, color, inline, size = 24, viewBox }, ref) => (
+  ({ children, className, color, inlineBlock, size = 24, viewBox }, ref) => (
     <svg
       ref={ref}
       className={className}
       color={color && `${theme[color]}`}
       fill="none"
       height={size}
-      style={{ display: inline ? 'inline-block' : 'block' }}
+      style={{ display: inlineBlock ? 'inline-block' : 'block' }}
       viewBox={viewBox || `0 0 24 24`}
       width={size}
       xmlns="http://www.w3.org/2000/svg"
--- src/types/IconProps.ts
+++ src/types/IconProps.ts
@@ -3,7 +3,7 @@ import type { Color } from 'src/types/Color';
 export type IconProps = {
   className?: string;
   color?: Color;
-  inline?: boolean;
+  inlineBlock?: boolean;
   /**
    * @default 24
    */
