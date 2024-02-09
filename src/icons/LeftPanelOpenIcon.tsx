1d6f586ec44166c8130077f9c14aedc08ad5adc1
Nick Lanners <nick.lanners@zonos.com>
2024-02-09 09:24:56 -0700

    feat(icons): new icons for Rates
    added the following icons for the Rates project:
    - LeftPanelClosed
    - LeftPanelOpen
    - RightPanelClosed
    - RightPanelOpen

---
+++ src/icons/LeftPanelOpenIcon.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react';
+
+import { IconBase } from 'src/icons/icon-base/_IconBase';
+import type { IconProps } from 'src/types/IconProps';
+
+export const LeftPanelOpenIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ className, color, inlineBlock, size }, ref) => (
+    <IconBase
+      ref={ref}
+      className={className}
+      color={color}
+      inlineBlock={inlineBlock}
+      size={size}
+      viewBox="0 0 24 24"
+    >
+      <path
+        clipRule="evenodd"
+        d="M21 7.75A2.75 2.75 0 0 0 18.25 5H5.75A2.75 2.75 0 0 0 3 7.75v8.5A2.75 2.75 0 0 0 5.75 19h12.5A2.75 2.75 0 0 0 21 16.25v-8.5ZM10 6.5v11h8.25c.69 0 1.25-.56 1.25-1.25v-8.5c0-.69-.56-1.25-1.25-1.25H10Z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </IconBase>
+  ),
+);
