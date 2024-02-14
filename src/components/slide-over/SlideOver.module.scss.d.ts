1333fb7d2f564e4026926efc4df63eec73df1ef7
Will Dickinson <will@zonos.com>
2024-02-14 11:58:58 -0700

    fix: types

--- src/components/slide-over/SlideOver.module.scss.d.ts
+++ src/components/slide-over/SlideOver.module.scss.d.ts
@@ -1,5 +1,4 @@
 export declare const footer: string;
-export declare const headerContent: string;
 export declare const slideOverContent: string;
 export declare const slideOverHeader: string;
 export declare const styledBaseDialog: string;
