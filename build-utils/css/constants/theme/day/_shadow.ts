2763ab93ac3f32276373a32e0363cf1685c88b53
cmccall8 <45436282+cmccall8@users.noreply.github.com>
2024-03-05 16:49:06 -0700

    Updated shadows

--- build-utils/css/constants/theme/day/_shadow.ts
+++ build-utils/css/constants/theme/day/_shadow.ts
@@ -1,12 +1,12 @@
 export const shadow = {
-  'v3-shadow-base': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 1px 2px rgba(10, 10, 11, 0.06), 0px 1px 3px rgba(10, 10, 11, 0.1)`,
-  'v3-shadow-medium': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 4px 6px rgba(10, 10, 11, 0.1), 0px 2px 4px rgba(10, 10, 11, 0.06)`,
-  'v3-shadow-large': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 10px 15px rgba(10, 10, 11, 0.1), 0px 4px 6px rgba(10, 10, 11, 0.05)`,
-  'v3-shadow-xl': `0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 20px 25px rgba(10, 10, 11, 0.1), 0px 10px 10px rgba(12, 12, 13, 0.04)`,
+  'v3-shadow-base': `0px 1px 3px 0px rgba(10, 10, 11, 0.1), 0px 1px 2px rgba(10, 10, 11, 0.06), 0px 0px 0px 1px rgba(110, 110, 120, 0.1)`,
+  'v3-shadow-medium': `0px 2px 4px 0px rgba(10, 10, 11, 0.06), 0px 4px 6px rgba(10, 10, 11, 0.1), 0px 0px 0px 1px rgba(110, 110, 120, 0.1)`,
+  'v3-shadow-large': `0px 4px 6px 0px rgba(10, 10, 11, 0.05), 0px 10px 15px rgba(10, 10, 11, 0.1), 0px 0px 0px 1px rgba(110, 110, 120, 0.1)`,
+  'v3-shadow-xl': `0px 10px 10px 0px rgba(12, 12, 13, 0.04), 0px 20px 25px rgba(10, 10, 11, 0.1), 0px 0px 0px 1px rgba(110, 110, 120, 0.1)`,
   'v3-shadow-xxl':
-    '0px 0px 0px 1px rgba(110, 110, 120, 0.1), 0px 32px 50px rgba(10, 10, 11, 0.24)',
+    '0px 32px 50px 0px rgba(10, 10, 11, 0.24), 0px 0px 0px 1px rgba(110, 110, 120, 0.1)',
   'v3-shadow-inset':
-    '0px 1px 0px #FFFFFF, 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 1px 1px rgba(0, 0, 0, 0.04), inset 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 0px 0px 0.5px rgba(0, 0, 0, 0.06), inset 0px 0px 8px rgba(0, 0, 0, 0.02)',
+    '0px 2px 4px rgba(0, 0, 0, 0.04), 0px 1px 0px 0px rgba(255, 255, 255, 1), inset 0px 0px 8px rgba(0, 0, 0, 0.02), inset 0px 0px 0px 0.5px rgba(0, 0, 0, 0.06), inset 0px 2px 4px rgba(0, 0, 0, 0.04), inset 0px 1px 1px rgba(0, 0, 0, 0.04)',
   'shadow-button-primary':
     '0px -1px 1px 0px rgba(0, 0, 0, 0.20) inset, 0px 1px 3px 0px rgba(68, 94, 238, 0.40)',
   'shadow-button-success':
