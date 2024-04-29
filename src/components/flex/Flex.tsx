64ebc1a5470752a0fab2f4d4cb60eab03f06b29c
ckgard27 <ckgard27@gmail.com>
2024-04-29 12:18:57 -0600

    fix(flex): added childrenFlex and flexWrap properties

--- src/components/flex/Flex.tsx
+++ src/components/flex/Flex.tsx
@@ -8,30 +8,49 @@ import styles from './Flex.module.scss';
 
 export type FlexProps = BaseProps & {
   /**
-   * @default 'stretch'
+   * @default 'center'
    */
   alignItems?: CSSProperties['alignItems'];
   children: ReactNode;
   /**
+   * @default 'initial'
+   * @description This is a shorthand for flex-grow, flex-shrink, and flex-basis.
+   */
+  childrenFlex?:
+    | 'auto'
+    | '1 0'
+    | '0 1'
+    | 'initial'
+    | 'inherit'
+    | 'unset'
+    // Combine with never to make sure string doesn't override the union
+    | (string & { _?: never });
+  /**
    * @default 'row'
    */
   flexDirection?: CSSProperties['flexDirection'];
+  flexWrap?: CSSProperties['flexWrap'];
   /**
    * @default 8
    */
   gap?: number;
   /**
-   * @default 'flex-start'
+   * @default 'space-between'
    */
   justifyContent?: CSSProperties['justifyContent'];
+  /**
+   * @default 0
+   */
   padding?: number;
 };
 
 export const Flex = ({
   alignItems = 'stretch',
   children,
+  childrenFlex = 'initial',
   className,
   flexDirection = 'row',
+  flexWrap = 'nowrap',
   gap = 8,
   justifyContent = 'flex-start',
   padding,
@@ -42,7 +61,9 @@ export const Flex = ({
     style={{
       ...style,
       '--dashboard-flex-wrapper-align-items': alignItems,
+      '--dashboard-flex-wrapper-flex-children': childrenFlex,
       '--dashboard-flex-wrapper-flex-direction': flexDirection,
+      '--dashboard-flex-wrapper-flex-wrap': flexWrap,
       '--dashboard-flex-wrapper-gap': `${gap}px`,
       '--dashboard-flex-wrapper-justify-content': justifyContent,
       '--dashboard-flex-wrapper-padding': `${padding || 0}px`,
