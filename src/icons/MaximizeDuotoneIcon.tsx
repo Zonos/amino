eb50cc25b50d5d33a4905954592dafbd9b5d9394
Nick Lanners <nick.lanners@zonos.com>
2024-10-03 16:37:18 -0600

    feat: new icons

---
+++ src/icons/MaximizeDuotoneIcon.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+
+import { IconBase } from 'src/icons/icon-base/_IconBase';
+import { theme } from 'src/styles/constants/theme';
+import type { Color } from 'src/types';
+import type { IconProps } from 'src/types/IconProps';
+
+export const MaximizeDuotoneIcon = forwardRef<
+  SVGSVGElement,
+  IconProps & { secondaryColor?: Color }
+>(({ className, color, inlineBlock, secondaryColor, size }, ref) => (
+  <IconBase
+    ref={ref}
+    className={className}
+    color={color || 'gray800'}
+    inlineBlock={inlineBlock}
+    size={size}
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M10.53 14.53a.75.75 0 1 0-1.06-1.06L5.5 17.44v-2.19a.75.75 0 0 0-1.5 0v4c0 .414.336.75.75.75h4a.75.75 0 0 0 0-1.5H6.56l3.97-3.97Z"
+      data-is-secondary-color="true"
+      fill={secondaryColor ? `${theme[secondaryColor]}` : `${theme.gray400}`}
+    />
+    <path
+      d="M13.47 9.47a.75.75 0 1 0 1.06 1.06l3.97-3.97v2.19a.75.75 0 0 0 1.5 0v-4a.75.75 0 0 0-.75-.75h-4a.75.75 0 0 0 0 1.5h2.19l-3.97 3.97Z"
+      fill="currentColor"
+    />
+  </IconBase>
+));
