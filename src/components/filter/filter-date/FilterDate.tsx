7506979e21348d03856a425b4c4497c5e30d5385
Nick Lanners <nick.lanners@zonos.com>
2024-12-27 17:10:25 -0700

    fix: date filter input bugs

--- src/components/filter/filter-date/FilterDate.tsx
+++ src/components/filter/filter-date/FilterDate.tsx
@@ -1,5 +1,7 @@
 import { type Dispatch, useCallback, useState } from 'react';
 
+import dayjs from 'dayjs';
+
 import { DateControls } from 'src/components/filter/filter-date/DateControls';
 import {
   type FilterDateAction,
@@ -13,6 +15,8 @@ import {
   type FilterApplyCallback,
   useFilterWrapper,
 } from 'src/components/filter/useFilterWrapper';
+
+import { defaultDateFormat } from './DateControlsWrapper';
 
 export type FilterDateProps = BaseFilterProps & {
   dispatch: Dispatch<FilterDateAction>;
@@ -37,6 +41,27 @@ export const FilterDate = ({
 
   const [editingFilterText, setEditingFilterText] = useState<string>('');
 
+  // adjusts the date to set the filter to be inclusive of the entered value
+  const getAdjustedDate = (date: string, type: 'begin' | 'end') => {
+    switch (rangeType) {
+      case 'is between':
+      case 'is equal to':
+        if (type === 'begin') {
+          return dayjs(date).subtract(1, 'days').format(defaultDateFormat);
+        }
+        if (type === 'end') {
+          return dayjs(date).add(1, 'days').format(defaultDateFormat);
+        }
+        return date;
+      case 'is on or after':
+        return dayjs(date).subtract(1, 'days').format(defaultDateFormat);
+      case 'is before or on':
+        return dayjs(date).add(1, 'days').format(defaultDateFormat);
+      default:
+        return date;
+    }
+  };
+
   const dispatchDateValues = useCallback(
     (value: FilterDateData) => {
       dispatch({
@@ -47,12 +72,15 @@ export const FilterDate = ({
       dispatch({
         name: 'dateData.dateBegin',
         type: 'change',
-        value: value.dateBegin || null,
+        value:
+          dayjs(value.dateBegin)
+            .subtract(1, 'days')
+            .format(defaultDateFormat) || null,
       });
       dispatch({
         name: 'dateData.dateEnd',
         type: 'change',
-        value: value.dateEnd || null,
+        value: getAdjustedDate(value.dateEnd || '', 'end'),
       });
       dispatch({
         name: 'dateData.lastCount',
@@ -77,12 +105,17 @@ export const FilterDate = ({
     dispatch({
       name: 'dateData.dateBegin',
       type: 'change',
-      value: editingValue.dateBegin || null,
+      value:
+        dayjs(editingValue.dateBegin)
+          .subtract(1, 'days')
+          .format(defaultDateFormat) || null,
     });
     dispatch({
       name: 'dateData.dateEnd',
       type: 'change',
-      value: editingValue.dateEnd || null,
+      value:
+        dayjs(editingValue.dateEnd).add(1, 'days').format(defaultDateFormat) ||
+        null,
     });
     dispatch({
       name: 'dateData.lastCount',
