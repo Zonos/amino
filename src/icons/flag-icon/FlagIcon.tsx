48121dc2bf74aecafd099f9f9ef4c7984545858a
Nick Lanners <nick.lanners@zonos.com>
2025-03-07 11:21:03 -0700

    fix: reset FlagIcon to previous implementation
    The current lazy/Suspense implementation is causing flickering and rendering loops in Dashboard

--- src/icons/flag-icon/FlagIcon.tsx
+++ src/icons/flag-icon/FlagIcon.tsx
@@ -1,49 +1,28 @@
-import { forwardRef, lazy, Suspense } from 'react';
+import { forwardRef } from 'react';
 
-import type * as flags from 'src/icons/flags/_FlagIndex';
-import { Default } from 'src/icons/flags/Default';
+import * as flags from 'src/icons/flags/_FlagIndex';
 
 export type Flag = keyof typeof flags;
 export type FlagScale = 'small' | 'medium' | 'large';
-
 export type FlagIconProps = { iconScale: FlagScale } & {
   code: Flag;
 };
 
 export const FlagIcon = forwardRef<SVGSVGElement, FlagIconProps>(
   ({ code, iconScale }, ref) => {
-    const flagSizeProps = () => {
-      switch (iconScale) {
-        case 'small':
-          return { height: 16, width: 16 };
-        case 'medium':
-          return { height: 20, width: 20 };
-        case 'large':
-        default:
-          return { borderRadius: 11, height: 32, width: 32 };
-      }
-    };
+    const Icon = flags[code];
+    // The props sometimes need to get typecast upstream, so we need to check
+    if (!Icon) {
+      return null;
+    }
 
-    const renderIcon = () => {
-      const Icon = lazy(() =>
-        // This exact string format is necessary to be used by the build script plugin for replacing dynamic imports
-        import(`src/icons/flags/` + code + `.tsx`).then(module => ({
-          default: module[code],
-        })),
-      );
+    if (iconScale === 'small') {
+      return <Icon ref={ref} height={16} width={16} />;
+    }
+    if (iconScale === 'medium') {
+      return <Icon ref={ref} height={20} width={20} />;
+    }
 
-      // The props sometimes need to get typecast upstream, so we need to check
-      if (!Icon) {
-        return null;
-      }
-
-      return <Icon ref={ref} {...flagSizeProps()} />;
-    };
-
-    return (
-      <Suspense fallback={<Default {...flagSizeProps()} />}>
-        {renderIcon()}
-      </Suspense>
-    );
+    return <Icon ref={ref} borderRadius={11} height={32} width={32} />;
   },
 );
