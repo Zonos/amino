5e05994ffbf0c8b239aae65eb52bbb4e51a304fe
Nick Lanners <nick.lanners@zonos.com>
2025-03-25 11:33:30 -0600

    test: setup for presentation

--- src/components/checkbox/__stories__/Checkbox.stories.tsx
+++ src/components/checkbox/__stories__/Checkbox.stories.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import type { Meta, StoryFn, StoryObj } from '@storybook/react';
+import { expect, userEvent, within } from '@storybook/test';
 
 import { Checkbox, type CheckboxProps } from 'src/components/checkbox/Checkbox';
 import { Flex } from 'src/components/flex/Flex';
@@ -44,6 +45,17 @@ export const BasicCheckbox: StoryObj<CheckboxProps> = {
     icon: <Default height={12} width={16} />,
     label: 'Input label',
     subtitle: 'Subtitle here',
+  },
+  play: async ({ canvasElement }) => {
+    // https://storybook.js.org/docs/writing-stories/play-function#working-with-the-canvas
+    // https://storybook.js.org/docs/writing-tests/component-testing
+    // https://testing-library.com/docs/
+    // https://storybook.js.org/docs/writing-tests/test-runner#disabling-tests
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('checkbox'), { delay: 500 });
+    expect(canvas.getByRole('checkbox')).toBeChecked();
+    await userEvent.click(canvas.getByRole('checkbox'), { delay: 500 });
+    expect(canvas.getByRole('checkbox')).not.toBeChecked();
   },
 };
 
