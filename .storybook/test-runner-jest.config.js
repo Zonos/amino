c3ca44e132edfd77d41b34935b7366371316d098
Nick Lanners <nick.lanners@zonos.com>
2025-03-28 09:47:11 -0600

    fix: increase default test timeout

---
+++ .storybook/test-runner-jest.config.js
@@ -0,0 +1,16 @@
+import { getJestConfig } from '@storybook/test-runner';
+
+// The default Jest configuration comes from @storybook/test-runner
+const testRunnerConfig = getJestConfig();
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+export default {
+  ...testRunnerConfig,
+  /** Add your own overrides below, and make sure
+   *  to merge testRunnerConfig properties with your own
+   * @see https://jestjs.io/docs/configuration
+   */
+  testTimeout: 30000,
+};
