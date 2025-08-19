894dc30ecfe70c2b336c8a5145156d26b24319b2
Thien Huynh <thien@zonos.com>
2025-08-19 15:08:57 -0600

    fix(style): update style utility to filter out empty string values and ensure all values are converted to strings

--- src/utils/style.ts
+++ src/utils/style.ts
@@ -4,12 +4,12 @@ import type { CSSProperties } from 'react';
  * Filter out all of the undefined values from the object so we don't have undefined css variable values
  */
 export const style = (
-  props: CSSProperties & { [key: string]: string | undefined },
+  props: CSSProperties & { [key: string]: string | number | undefined },
 ) =>
   // loop through the object and filter out all of the undefined values
   Object.entries(props).reduce((acc: Record<string, string>, [key, value]) => {
-    if (value !== undefined) {
-      acc[key] = value;
+    if (value !== undefined && value !== '') {
+      acc[key] = value.toString();
     }
     return acc;
   }, {});
